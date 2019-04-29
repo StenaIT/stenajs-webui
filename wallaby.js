f2c4cff3f1dbe5408ebe09910e1551668ce7aeee
Mattias Andersson <mattias800@gmail.com>
2019-04-29 13:30:25 +0200

    Update wallaby config.

--- wallaby.js
+++ wallaby.js
@@ -1,25 +1,21 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = function (wallaby) {
+module.exports = function(wallaby) {
   return {
     files: [
-      'action-center-framework/src/**/*.ts',
-      'action-center-app/src/**/*.ts',
-      {pattern: '*/src/**/__tests__/**/*.test.ts', ignore: true}
+      "packages/**/src/**/*.ts",
+      { pattern: "packages/**/src/**/__tests__/**/*.test.ts", ignore: true }
     ],
-    tests: [
-      'action-center-framework/src/**/__tests__/**/*.test.ts',
-      'action-center-app/src/**/__tests__/**/*.test.ts'
-    ],
+    tests: ["packages/**/src/**/__tests__/**/*.test.ts"],
     env: {
-      type: 'node'
+      type: "node"
     },
-    testFramework: 'jest',
+    testFramework: "jest",
     compilers: {
-      '**/*.ts?(x)': wallaby.compilers.typeScript({
-        module: 'commonjs',
+      "**/*.ts?(x)": wallaby.compilers.typeScript({
+        module: "commonjs",
         target: "es5"
       })
-    },
+    }
   };
 };
