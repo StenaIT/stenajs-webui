232233c408ff6aa6ad28512d61bacdd1a569257e
Mattias Andersson <mattias800@gmail.com>
2019-04-29 13:24:32 +0200

    Add useTextTheme hook.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,6 @@
-export * from "./Box/Box";
+export * from "./components/box/Box";
+export * from "./components/text/variants/LargeText";
+export * from "./components/text/variants/SmallerText";
+export * from "./components/text/variants/StandardText";
+export * from "./components/text/variants/SmallText";
+export * from "./components/text/variants/TinyText";
--- packages/core/src/theme/DefaultTheme.ts
+++ packages/core/src/theme/DefaultTheme.ts
@@ -1,15 +1,14 @@
-import { defaultColors } from './default-values/DefaultColors';
-import { defaultFonts } from './default-values/DefaultFonts';
-import { defaultFontSizes } from './default-values/DefaultFontSizes';
-import { defaultFontWeights } from './default-values/DefaultFontWeights';
-import { defaultMetrics } from './default-values/DefaultMetrics';
-import { Theme } from './Theme';
+import { defaultColors } from "./default-values/DefaultColors";
+import { defaultFonts } from "./default-values/DefaultFonts";
+import { defaultFontSizes } from "./default-values/DefaultFontSizes";
+import { defaultFontWeights } from "./default-values/DefaultFontWeights";
+import { defaultMetrics } from "./default-values/DefaultMetrics";
+import { Theme } from "./Theme";
 
 export const defaultTheme: Theme = {
   colors: defaultColors,
   metrics: defaultMetrics,
   fontWeights: defaultFontWeights,
   fonts: defaultFonts,
-  fontSizes: defaultFontSizes,
-  components: {},
+  fontSizes: defaultFontSizes
 };
