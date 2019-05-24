572a0eaa169d39fe1d35e0a84575f7a4c527a778
Mattias Andersson <mattias800@gmail.com>
2019-05-24 10:49:48 +0200

    Backgrounds addon now working.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,10 @@
-import { withInfo } from "@storybook/addon-info";
-import { withKnobs } from "@storybook/addon-knobs";
-import { addDecorator, addParameters, configure } from "@storybook/react";
-import { StenaTheme } from "./stena-theme";
-import { withA11y } from "@storybook/addon-a11y";
+import {withBackgrounds} from "@storybook/addon-backgrounds";
+import {withInfo} from "@storybook/addon-info";
+import {withKnobs} from "@storybook/addon-knobs";
+import {addDecorator, addParameters, configure} from "@storybook/react";
+import {StenaTheme} from "./stena-theme";
+
+import {withA11y} from "@storybook/addon-a11y"; // Must be imported last, otherwise we get weird error.
 
 function loadStories() {
   const req = require.context("../", true, /\.stories\.tsx$/);
@@ -16,6 +18,17 @@ addParameters({
   }
 });
 
+addParameters({
+  backgrounds: [
+    { name: "White", value: "#ffffff", default: true },
+    { name: "Dark", value: "#226F81" },
+    { name: "Stena blue", value: "#2378cd" },
+    { name: "Stena green", value: "#60bd2f" },
+    { name: "Twitter", value: "#00aced" },
+    { name: "Facebook", value: "#3b5998" }
+  ]
+});
+
 addDecorator(
   withInfo({
     header: false
@@ -23,5 +36,6 @@ addDecorator(
 );
 addDecorator(withKnobs);
 addDecorator(withA11y);
+addDecorator(withBackgrounds);
 
 configure(loadStories, module);
