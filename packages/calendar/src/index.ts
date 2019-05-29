64a9c5a202ec21583e60538cb699f84be7562e44
Mattias Andersson <mattias800@gmail.com>
2019-05-29 11:01:39 +0200

    Add more exports to calendar.

--- packages/calendar/src/index.ts
+++ packages/calendar/src/index.ts
@@ -5,3 +5,4 @@ export * from "./components/ui/date-time-input/DateInput";
 export * from "./components/ui/date-time-input/DateRangeInput";
 export * from "./components/ui/date-time-input/DateTextInput";
 export * from "./components/ui/date-time-input/TimeTextInput";
+export * from "./components/ui/calendar/util/CalendarIntervalValidator";
