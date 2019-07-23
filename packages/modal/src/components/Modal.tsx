3fb3be8627e023939a69b13216274d4801c8dfad
Johan Lindskogen <johan.lindskogen@gmail.com>
2019-07-23 17:20:58 +0200

    Add modal animation

--- packages/modal/src/components/Modal.tsx
+++ packages/modal/src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import { ClassNames } from "@emotion/core";
+import { ClassNames, keyframes } from "@emotion/core";
 import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes";
 import { Row, SeparatorLine, Spacing } from "@stenajs-webui/core";
 import { StandardButton } from "@stenajs-webui/elements";
@@ -6,6 +6,26 @@ import { createStandardButtonTheme } from "@stenajs-webui/theme";
 import * as React from "react";
 import { ReactNode } from "react";
 import * as ReactModal from "react-modal";
+
+const modalAnimateIn = keyframes`
+  0% {
+      transform: translateY(-20px);
+      opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+`;
+
+const fadeIn = keyframes`
+  0% {
+     background-color: rgba(0, 0, 0, 0);
+  }
+  100% {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+`;
 
 export const BaseModal: React.FC<ReactModal.Props> = props => {
   return (
@@ -27,6 +47,8 @@ export const BaseModal: React.FC<ReactModal.Props> = props => {
             justify-content: flex-start;
             align-items: center;
 
+            animation: ${fadeIn} 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+
             @media print {
               background-color: rgba(255, 255, 255, 1);
             }
@@ -45,6 +67,9 @@ export const BaseModal: React.FC<ReactModal.Props> = props => {
             box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.15);
 
             width: 900px;
+
+            animation: ${modalAnimateIn} 0.3s
+              cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
 
             @media print {
               box-shadow: none;
