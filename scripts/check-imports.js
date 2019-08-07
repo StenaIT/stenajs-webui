86dfb592f75476f90a4be31e193d620c93729331
Mattias Andersson <mattias800@gmail.com>
2019-08-07 15:13:34 +0200

    Add checker that checks that all imported modules in a package, are specified as peerDependency or dependency.

--- scripts/check-imports.js
+++ scripts/check-imports.js
@@ -117,6 +117,8 @@ const checkImport = (
     }
     if (imported.startsWith(".")) {
       checkIfImportGoesToPackagesFolder(filePath, packageJson, imported);
+    } else {
+      checkIfImportIsPackageJsonDependency(filePath, packageJson, imported);
     }
   }
 };
@@ -139,4 +141,32 @@ const checkIfImportGoesToPackagesFolder = (filePath, packageJson, imported) => {
       success = false;
     }
   });
+};
+
+const checkIfImportIsPackageJsonDependency = (
+  filePath,
+  packageJson,
+  imported
+) => {
+  const parts = imported.split("/");
+  const size = parts.length;
+  for (let i = 0; i < size; i++) {
+    const moduleNameToCheck = parts.join("/");
+    if (
+      packageJson.dependencies[moduleNameToCheck] ||
+      packageJson.peerDependencies[moduleNameToCheck]
+    ) {
+      return;
+    }
+    parts.pop();
+  }
+
+  console.log(
+    `ERROR: ${
+      packageJson.name
+    } import error: Trying to import module that is not specified as dependency:`
+  );
+  console.log(filePath);
+  console.log(`import from '${imported}'`);
+  success = false;
 };
