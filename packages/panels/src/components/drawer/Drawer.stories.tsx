e88fe80a0b8ca8760f3c0eac8cb2b19dafc20147
knegusen <knegusen@gmail.com>
2019-08-22 12:44:06 +0200

    Remove color knob from stories

--- packages/panels/src/components/drawer/Drawer.stories.tsx
+++ packages/panels/src/components/drawer/Drawer.stories.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@stenajs-webui/core";
 import { Drawer } from "@stenajs-webui/panels";
-import { boolean, color, number } from "@storybook/addon-knobs";
+import { boolean, number } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
@@ -14,23 +14,15 @@ storiesOf("panels/Drawer", module)
   ))
   .add("from right", () => (
     <Drawer isOpen={boolean("open", false)} slideFrom={"right"}>
-      <Box
-        background={color("Background color", "#9198e5")}
-        spacing={2}
-        indent={2}
-      >
+      <Box background={"#9198e5"} spacing={2} indent={2}>
         Drawer content
       </Box>
     </Drawer>
   ))
   .add("using portal", () => (
     <Drawer isOpen={boolean("open", false)} portalTarget={document.body}>
-      <Box
-        background={color("Background color", "#9198e5")}
-        spacing={2}
-        indent={2}
-      >
-        Drawer content korv
+      <Box background={"#9198e5"} spacing={2} indent={2}>
+        Drawer content
       </Box>
     </Drawer>
   ));
