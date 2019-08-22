178589aee98bc30c5a863bc30f7841139819b419
knegusen <knegusen@gmail.com>
2019-08-22 12:25:48 +0200

    Add Drawer

---
+++ packages/panels/src/components/drawer/Drawer.stories.tsx
@@ -0,0 +1,29 @@
+import { Box } from "@stenajs-webui/core";
+import { Drawer } from "@stenajs-webui/panels";
+import { boolean, color } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+storiesOf("panels/Drawer", module)
+  .add("standard", () => (
+    <Drawer isOpen={boolean("open", false)}>
+      <Box
+        background={color("Background color", "#9198e5")}
+        spacing={2}
+        indent={2}
+      >
+        Drawer content
+      </Box>
+    </Drawer>
+  ))
+  .add("from right", () => (
+    <Drawer isOpen={boolean("open", false)} slideFrom={"right"}>
+      <Box
+        background={color("Background color", "#9198e5")}
+        spacing={2}
+        indent={2}
+      >
+        Drawer content
+      </Box>
+    </Drawer>
+  ));
---
+++ packages/panels/src/components/drawer/Drawer.tsx
@@ -0,0 +1,55 @@
+import styled from "@emotion/styled";
+import * as React from "react";
+
+type SlideFrom = "left" | "right";
+
+export interface DrawerProps {
+  drawerRef?: React.Ref<HTMLDivElement>;
+  isOpen: boolean;
+  slideFrom?: SlideFrom;
+  width?: string;
+  zIndex?: number;
+}
+
+type DrawerWrapperProps = Pick<
+  DrawerProps,
+  "isOpen" | "slideFrom" | "width" | "zIndex"
+>;
+
+export const DrawerWrapper = styled("div")<DrawerWrapperProps>`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  transform: translateX(
+    ${({ isOpen, slideFrom, width }) =>
+      isOpen ? 0 : slideFrom === "right" ? `${width}` : `-${width}`}
+  );
+  right: ${props => (props.slideFrom === "right" ? 0 : "auto")};
+  left: ${props => (props.slideFrom === "left" ? 0 : "auto")};
+  height: 100%;
+  width: ${({ width }) => width};
+  transition: 0.6s all;
+  box-shadow: ${({ isOpen }) =>
+    isOpen ? "2px 2px 20px 0 rgba(0, 0, 0, 0.15)" : "none"};
+  z-index: ${({ zIndex }) => zIndex};
+  top: 0;
+`;
+
+export const Drawer: React.FC<DrawerProps> = ({
+  children,
+  drawerRef,
+  isOpen,
+  slideFrom = "left",
+  width = "400px",
+  zIndex = 1000
+}) => (
+  <DrawerWrapper
+    ref={drawerRef}
+    isOpen={isOpen}
+    slideFrom={slideFrom}
+    width={width}
+    zIndex={zIndex}
+  >
+    {children}
+  </DrawerWrapper>
+);
