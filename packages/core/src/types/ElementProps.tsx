83f60248d6ae3da1cf7f0c3389de0d315093c157
Mattias Andersson <mattias800@gmail.com>
2019-09-24 14:54:40 +0200

    Using Ref in ElementProps instead of RefObject, except for inputRef for input elements.

--- packages/core/src/types/ElementProps.tsx
+++ packages/core/src/types/ElementProps.tsx
@@ -1,28 +1,28 @@
-import { RefObject } from "react";
+import { Ref, RefObject } from "react";
 
 export interface InputProps<T = any>
   extends Omit<JSX.IntrinsicElements["input"], "ref" | "value" | "onChange"> {
   /** Input ref to use. If omitted, an internal ref will be used. */
-  inputRef?: RefObject<HTMLInputElement>;
-  wrapperRef?: RefObject<T>;
+  inputRef?: RefObject<HTMLInputElement>; // This is a ref object, since the input components use ref.current which Ref type doesn't have.
+  wrapperRef?: Ref<T>;
 }
 
 export interface DivProps extends Omit<JSX.IntrinsicElements["div"], "ref"> {
   /** Input ref to use. If omitted, an internal ref will be used. */
-  innerRef?: RefObject<HTMLDivElement>;
+  innerRef?: Ref<HTMLDivElement>;
 }
 
 export interface SpanProps extends Omit<JSX.IntrinsicElements["span"], "ref"> {
   /** Input ref to use. If omitted, an internal ref will be used. */
-  innerRef?: RefObject<HTMLSpanElement>;
+  innerRef?: Ref<HTMLSpanElement>;
 }
 
 export interface ButtonProps
   extends Omit<JSX.IntrinsicElements["button"], "ref"> {
   /** Input ref to use. If omitted, an internal ref will be used. */
-  innerRef?: RefObject<HTMLButtonElement>;
+  innerRef?: Ref<HTMLButtonElement>;
 }
 
 export interface WithInnerRef<TInputElement> {
-  innerRef?: RefObject<TInputElement>;
+  innerRef?: Ref<TInputElement>;
 }
