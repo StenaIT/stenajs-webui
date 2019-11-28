a12fdfeec0f6bcda25a66c09f23c9c5fa3190779
Mattias Andersson <mattias800@gmail.com>
2019-11-28 09:11:20 +0100

    Extract rollup config into centralized provider. Switch now works in Storybook.

--- packages/modal/rollup.config.js
+++ packages/modal/rollup.config.js
@@ -1,43 +1,6 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import url from "rollup-plugin-url";
-import svgr from "@svgr/rollup";
-import postcss from "rollup-plugin-postcss";
+import { createRollupConfig } from "../../rollup.config.provider";
 import pkg from "./package.json";
-const postcssPresetEnv = require('postcss-preset-env');
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    postcss({
-      plugins: [postcssPresetEnv({ stage: 0 })],
-      modules: true
-    }),
-external({
-      includeDependencies: true
-    }),
-    url(),
-    svgr(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true,
-      typescript: require("typescript")
-    }),
-    commonjs()
-  ]
-};
+const config = createRollupConfig(pkg);
+
+export default config;
--- packages/panels/rollup.config.js
+++ packages/panels/rollup.config.js
@@ -1,44 +1,8 @@
-import svgr from "@svgr/rollup";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import typescript from "rollup-plugin-typescript2";
-import url from "rollup-plugin-url";
-import postcss from "rollup-plugin-postcss";
+import { createRollupConfig } from "../../rollup.config.provider";
 import pkg from "./package.json";
-const postcssPresetEnv = require('postcss-preset-env');
 
-export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named",
-      sourcemap: true
-    }
-  ],
-  external: ["@emotion/styled", "date-fns"],
-  plugins: [
-    postcss({
-      plugins: [postcssPresetEnv({ stage: 0 })],
-      modules: true
-    }),
-    external({
-      includeDependencies: true
-    }),
-    url(),
-    svgr(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true,
-      typescript: require("typescript")
-    }),
-    commonjs()
-  ]
-};
+const config = createRollupConfig(pkg, {
+  external: ["@emotion/styled", "date-fns"]
+});
+
+export default config;
