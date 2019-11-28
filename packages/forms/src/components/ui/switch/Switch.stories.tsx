ab85daeeba7aca789475578f203b81065ea4fb51
Mattias Andersson <mattias800@gmail.com>
2019-11-28 13:50:04 +0100

    Rewrite Switch stories in new story format.

--- packages/forms/src/components/ui/switch/Switch.stories.tsx
+++ packages/forms/src/components/ui/switch/Switch.stories.tsx
@@ -1,38 +1,28 @@
-import {
-  defaultSwitchTheme,
-  Switch,
-  SwitchWithLabel
-} from "@stenajs-webui/forms";
+import { defaultSwitchTheme, Switch } from "@stenajs-webui/forms";
 import * as knobs from "@storybook/addon-knobs";
-import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
-storiesOf("forms/Switch", module)
-  .add("standard", () => (
-    <Switch
-      value={knobs.boolean("Toggled", false)}
-      disabled={knobs.boolean("Disabled", false)}
-    />
-  ))
-  .add("custom size", () => (
-    <Switch
-      value={knobs.boolean("Toggled", false)}
-      disabled={knobs.boolean("Disabled", false)}
-      theme={{
-        ...defaultSwitchTheme,
-        height: 40,
-        width: 200
-      }}
-    />
-  ))
-  .add("disabled", () => (
-    <Switch value={knobs.boolean("Toggled", false)} disabled />
-  ));
+export default { title: "forms/Switch" };
 
-storiesOf("forms/Switch/SwitchWithLabel", module).add("standard", () => (
-  <SwitchWithLabel
-    label={knobs.text("Label", "Enable something")}
+export const standard = () => (
+  <Switch
     value={knobs.boolean("Toggled", false)}
     disabled={knobs.boolean("Disabled", false)}
   />
-));
+);
+
+export const customSize = () => (
+  <Switch
+    value={knobs.boolean("Toggled", false)}
+    disabled={knobs.boolean("Disabled", false)}
+    theme={{
+      ...defaultSwitchTheme,
+      height: 40,
+      width: 200
+    }}
+  />
+);
+
+export const disabled = () => (
+  <Switch value={knobs.boolean("Toggled", false)} disabled />
+);
---
+++ packages/forms/src/components/ui/switch/SwitchWithLabel.stories.tsx
@@ -0,0 +1,13 @@
+import { SwitchWithLabel } from "@stenajs-webui/forms";
+import * as knobs from "@storybook/addon-knobs";
+import * as React from "react";
+
+export default { title: "forms/Switch/SwitchWithLabel" };
+
+export const standard = () => (
+  <SwitchWithLabel
+    label={knobs.text("Label", "Enable something")}
+    value={knobs.boolean("Toggled", false)}
+    disabled={knobs.boolean("Disabled", false)}
+  />
+);
