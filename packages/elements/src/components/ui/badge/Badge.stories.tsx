7232e4845a36ffd4ad625b7d4e1ff8e98d73ae21
Mattias Andersson <mattias800@gmail.com>
2020-02-11 15:04:05 +0100

    Add override size knob, to test that component falls back to CSS vars when size is no longer specified.

--- packages/elements/src/components/ui/badge/Badge.stories.tsx
+++ packages/elements/src/components/ui/badge/Badge.stories.tsx
@@ -1,14 +1,20 @@
-import { color, number } from "@storybook/addon-knobs";
+import { boolean, color, number } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import { Badge } from "@stenajs-webui/elements";
 
 storiesOf("elements/Badge/Badge", module)
   .add("standard", () => <Badge label={5} />)
-  .add("with knobs", () => (
-    <Badge
-      label={number("Label", 5)}
-      color={color("Color", "red")}
-      textColor={color("Text color", "white")}
-    />
-  ));
+  .add("with knobs", () => {
+    const overrideSize = boolean("Override size", false);
+    const size =
+      number("Size", 18, { range: true, min: 10, max: 25, step: 1 }) + "px";
+    return (
+      <Badge
+        label={number("Label", 5)}
+        color={color("Color", "red")}
+        textColor={color("Text color", "white")}
+        size={overrideSize ? size : undefined}
+      />
+    );
+  });
