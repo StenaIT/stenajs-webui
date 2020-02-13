c3af073ad02ff36c05472b879994b0efcbc72ee7
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-02-13 21:53:21 +0100

    Loading and success states

--- packages/elements/src/components/ui/buttons/NewButton.stories.tsx
+++ packages/elements/src/components/ui/buttons/NewButton.stories.tsx
@@ -1,8 +1,13 @@
 import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons/faCoffee";
 import { Column, Row } from "@stenajs-webui/core";
-import { NewButton, ButtonSize } from "@stenajs-webui/elements";
+import {
+  ButtonSize,
+  NewButton,
+  SecondaryButton
+} from "@stenajs-webui/elements";
 import { action } from "@storybook/addon-actions";
+import { boolean } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
@@ -10,62 +15,118 @@ const buttonSizes: ButtonSize[] = ["small", "normal", "large"];
 
 storiesOf("elements/Buttons/NewButton", module)
   .add("default", () => (
-    <Column alignItems={"flex-start"}>
-      {buttonSizes.map(size => (
-        <Row key={size}>
-          <NewButton
-            size={size}
-            label={"Submit"}
-            onClick={action("Button clicked")}
-          />
-          <NewButton
-            size={size}
-            label={"Submit"}
-            disabled
-            onClick={action("Button clicked")}
-          />
-          <NewButton
-            size={size}
-            leftIcon={faCoffee}
-            onClick={action("Button clicked")}
-          />
-          <NewButton
-            size={size}
-            label={"Submit"}
-            leftIcon={faCoffee}
-            onClick={action("Button clicked")}
-          />
-          <NewButton
-            size={size}
-            label={"Submit"}
-            rightIcon={faCoffee}
-            onClick={action("Button clicked")}
-          />
-          <NewButton
-            size={size}
-            label={"Submit"}
-            leftIcon={faCheck}
-            rightIcon={faCoffee}
-            onClick={action("Button clicked")}
-          />
-        </Row>
+    <>
+      {[NewButton, SecondaryButton].map(ButtonVariant => (
+        <Column alignItems={"flex-start"}>
+          {buttonSizes.map(size => (
+            <Row key={size}>
+              <ButtonVariant
+                size={size}
+                label={"Submit"}
+                onClick={action("Button clicked")}
+              />
+              <ButtonVariant
+                size={size}
+                label={"Submit"}
+                disabled
+                onClick={action("Button clicked")}
+              />
+              <ButtonVariant
+                size={size}
+                leftIcon={faCoffee}
+                onClick={action("Button clicked")}
+              />
+              <ButtonVariant
+                size={size}
+                label={"Submit"}
+                leftIcon={faCoffee}
+                onClick={action("Button clicked")}
+              />
+              <ButtonVariant
+                size={size}
+                label={"Submit"}
+                rightIcon={faCoffee}
+                onClick={action("Button clicked")}
+              />
+              <ButtonVariant
+                size={size}
+                label={"Submit"}
+                leftIcon={faCheck}
+                rightIcon={faCoffee}
+                onClick={action("Button clicked")}
+              />
+            </Row>
+          ))}
+        </Column>
       ))}
-    </Column>
+    </>
   ))
   .add("with loading", () => (
-    <NewButton label={"Submit"} loading onClick={action("Button clicked")} />
+    <>
+      {[NewButton, SecondaryButton].map(ButtonVariant => (
+        <Row alignItems={"flex-start"}>
+          {buttonSizes.map(size => (
+            <ButtonVariant
+              key={size}
+              size={size}
+              label={"Submit"}
+              loading={boolean("Loading", true)}
+              onClick={action("Button clicked")}
+            />
+          ))}
+        </Row>
+      ))}
+    </>
   ))
   .add("with loading label", () => (
-    <NewButton
-      label={"Submit"}
-      loading
-      loadingLabel={"Loading..."}
-      onClick={action("Button clicked")}
-    />
+    <>
+      {[NewButton, SecondaryButton].map(ButtonVariant => (
+        <Row alignItems={"flex-start"}>
+          {buttonSizes.map(size => (
+            <ButtonVariant
+              key={size}
+              size={size}
+              label={"Submit"}
+              loading={boolean("Loading", true)}
+              loadingLabel={"Loading..."}
+              onClick={action("Button clicked")}
+            />
+          ))}
+        </Row>
+      ))}
+    </>
   ))
   .add("with success", () => (
-    <NewButton label={"Submit"} success onClick={action("Button clicked")} />
+    <>
+      {[NewButton, SecondaryButton].map(ButtonVariant => (
+        <Row alignItems={"flex-start"}>
+          {buttonSizes.map(size => (
+            <ButtonVariant
+              key={size}
+              size={size}
+              label={"Submit"}
+              success={boolean("Success", true)}
+              onClick={action("Button clicked")}
+            />
+          ))}
+        </Row>
+      ))}
+    </>
   ))
   .add("with success label", () => (
-    <NewButton label={"Submit"} success successLabel={"Done!"} />
+    <>
+      {[NewButton, SecondaryButton].map(ButtonVariant => (
+        <Row alignItems={"flex-start"}>
+          {buttonSizes.map(size => (
+            <ButtonVariant
+              key={size}
+              size={size}
+              label={"Submit"}
+              success={boolean("Success", true)}
+              successLabel={"Done!"}
+            />
+          ))}
+        </Row>
+      ))}
+    </>
   ));
--- packages/elements/src/components/ui/buttons/NewButton.tsx
+++ packages/elements/src/components/ui/buttons/NewButton.tsx
@@ -1,30 +1,89 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import cx from "classnames";
 import * as React from "react";
 import { MouseEventHandler } from "react";
+import { InputSpinner } from "../../..";
 import styles from "./NewButton.module.css";
 
 export type ButtonSize = "normal" | "small" | "large";
-export const NewButton: React.FC<{
+
+export interface NewButtonProps {
   label: string;
+  loadingLabel?: string;
+  className?: string;
   size?: ButtonSize;
+  loading?: boolean;
+  success?: boolean;
+  successLabel?: string;
   disabled?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   leftIcon?: IconDefinition;
   rightIcon?: IconDefinition;
-}> = ({ label, size = "normal", disabled, leftIcon, rightIcon, onClick }) => (
-  <button
-    onClick={onClick}
-    className={cx(styles.button, styles[size], !label && styles.iconButton)}
-    disabled={disabled}
-  >
-    {leftIcon && (
-      <FontAwesomeIcon icon={leftIcon} className={styles.iconLeft} />
-    )}
-    {label && <span>{label}</span>}
-    {rightIcon && (
-      <FontAwesomeIcon icon={rightIcon} className={styles.iconRight} />
-    )}
-  </button>
-);
+}
+
+const getButtonLabel = (
+  label: string,
+  success: boolean,
+  successLabel: string | undefined,
+  loading: boolean,
+  loadingLabel: string | undefined
+): string | null => {
+  if (success) {
+    return successLabel ?? null;
+  } else if (loading) {
+    return loadingLabel ?? null;
+  } else {
+    return label;
+  }
+};
+
+export const NewButton: React.FC<NewButtonProps> = ({
+  label,
+  className,
+  size = "normal",
+  loadingLabel,
+  loading = false,
+  success = false,
+  successLabel,
+  disabled,
+  leftIcon,
+  rightIcon,
+  onClick
+}) => {
+  const buttonLabel = getButtonLabel(
+    label,
+    success,
+    successLabel,
+    loading,
+    loadingLabel
+  );
+  return (
+    <button
+      onClick={onClick}
+      className={cx(
+        styles.button,
+        styles[size],
+        !label && styles.iconButton,
+        className
+      )}
+      disabled={disabled}
+    >
+      {success ? (
+        <FontAwesomeIcon icon={faCheck} className={styles.iconLeft} />
+      ) : loading ? (
+        <div className={styles.iconLeft}>
+          <InputSpinner size={"100%"} />
+        </div>
+      ) : leftIcon ? (
+        <FontAwesomeIcon icon={leftIcon} className={styles.iconLeft} />
+      ) : null}
+
+      {buttonLabel && <span>{buttonLabel}</span>}
+      {rightIcon && (
+        <FontAwesomeIcon icon={rightIcon} className={styles.iconRight} />
+      )}
+    </button>
+  );
+};
