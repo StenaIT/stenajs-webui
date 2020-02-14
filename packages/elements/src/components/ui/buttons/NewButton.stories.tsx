96ba451092529eae5c6dfa04629353ca1ead8408
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-02-14 13:41:56 +0100

    Fix import

--- packages/elements/src/components/ui/buttons/NewButton.stories.tsx
+++ packages/elements/src/components/ui/buttons/NewButton.stories.tsx
@@ -4,13 +4,13 @@ import { Column, Row } from "@stenajs-webui/core";
 import {
   ButtonSize,
   NewButton,
+  Icon,
   SecondaryButton
 } from "@stenajs-webui/elements";
 import { action } from "@storybook/addon-actions";
 import { boolean } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
-import { Icon } from "../../..";
 
 const buttonSizes: ButtonSize[] = ["small", "normal", "large"];
 
