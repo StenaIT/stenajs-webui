036839dcdeb0e37e50aaab3d11622453d3b4d27f
Niklas Tegnander <niklas.tegnander@stenaline.com>
2020-03-09 15:58:53 +0100

    Chip with two buttons

--- packages/elements/src/components/ui/chip/Chip.stories.tsx
+++ packages/elements/src/components/ui/chip/Chip.stories.tsx
@@ -5,12 +5,12 @@ import * as React from "react";
 storiesOf("elements/Chip", module).add("standard", () => (
   <div className={"margin-wrapper"}>
     <Chip label={"Default"} />
-    <Chip label={"With onClickLabel"} onClick={() => alert("hello world")} />
-    <Chip label={"With onClickRemove"} onClickRemove={() => alert("remove")} />
+    <Chip label={"With onClick"} onClick={() => alert("hello world")} />
+    <Chip label={"With onRemove"} onRemove={() => alert("remove")} />
     <Chip
       label={"With both"}
       onClick={() => alert("hello world")}
-      onClickRemove={() => alert("remove")}
+      onRemove={() => alert("remove")}
     />
   </div>
 ));
--- packages/elements/src/components/ui/chip/Chip.tsx
+++ packages/elements/src/components/ui/chip/Chip.tsx
@@ -1,33 +1,36 @@
 import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes";
-import { Row } from "@stenajs-webui/core";
 import * as React from "react";
 import { PrimaryButton, PrimaryButtonProps } from "../buttons/PrimaryButton";
 import styles from "./Chip.module.css";
 
 export interface ChipProps extends PrimaryButtonProps {
-  onClickRemove?: () => void;
+  onRemove?: () => void;
 }
 
 export const Chip: React.FC<ChipProps> = ({
-  onClickRemove,
+  onRemove,
   className,
   label,
   onClick,
   ...props
 }) => {
   return (
-    <Row className={styles.chip}>
+    <div className={styles.chip}>
       <PrimaryButton
         label={label}
         {...props}
         size={"small"}
         className={styles.label}
+        onClick={onClick}
       />
-      <PrimaryButton
-        leftIcon={faTimes}
-        size={"small"}
-        className={styles.close}
-      />
-    </Row>
+      {onRemove && (
+        <PrimaryButton
+          leftIcon={faTimes}
+          size={"small"}
+          className={styles.close}
+          onClick={onRemove}
+        />
+      )}
+    </div>
   );
 };
