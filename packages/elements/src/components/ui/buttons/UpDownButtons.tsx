3d5ab1014cdf0818eed6881c22145e51919d14d4
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-06-04 21:17:25 +0200

    Set align-self on UpDownButtons

--- packages/elements/src/components/ui/buttons/UpDownButtons.tsx
+++ packages/elements/src/components/ui/buttons/UpDownButtons.tsx
@@ -1,14 +1,13 @@
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons/faCaretDown";
 import { faCaretUp } from "@fortawesome/free-solid-svg-icons/faCaretUp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Box, Column, Row } from "@stenajs-webui/core";
+import { Column, Row } from "@stenajs-webui/core";
 import * as React from "react";
 import styles from "./UpDownButtons.module.css";
 
 export interface UpDownButtonsProps {
   onClickUp?: () => void;
   onClickDown?: () => void;
-  buttonHeight?: string;
   iconColor?: string;
   disabled?: boolean;
 }
@@ -16,43 +15,30 @@ export interface UpDownButtonsProps {
 export const UpDownButtons: React.FC<UpDownButtonsProps> = ({
   onClickDown,
   onClickUp,
-  buttonHeight,
   iconColor,
   disabled
 }) => (
   <Column className={styles.upDownButtons}>
     <button onClick={onClickUp} className={styles.button} disabled={disabled}>
-      <Row
-        height={buttonHeight}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Box indent={0.75}>
-          <FontAwesomeIcon
-            icon={faCaretUp}
-            size={"sm"}
-            color={
-              disabled ? "var(--swui-textinput-text-color-disabled)" : iconColor
-            }
-          />
-        </Box>
+      <Row justifyContent={"center"} alignItems={"center"}>
+        <FontAwesomeIcon
+          icon={faCaretUp}
+          size={"sm"}
+          color={
+            disabled ? "var(--swui-textinput-text-color-disabled)" : iconColor
+          }
+        />
       </Row>
     </button>
     <button onClick={onClickDown} className={styles.button} disabled={disabled}>
-      <Row
-        height={buttonHeight}
-        justifyContent={"center"}
-        alignItems={"center"}
-      >
-        <Box indent={0.75}>
-          <FontAwesomeIcon
-            icon={faCaretDown}
-            size={"sm"}
-            color={
-              disabled ? "var(--swui-textinput-text-color-disabled)" : iconColor
-            }
-          />
-        </Box>
+      <Row justifyContent={"center"} alignItems={"center"}>
+        <FontAwesomeIcon
+          icon={faCaretDown}
+          size={"sm"}
+          color={
+            disabled ? "var(--swui-textinput-text-color-disabled)" : iconColor
+          }
+        />
       </Row>
     </button>
   </Column>
