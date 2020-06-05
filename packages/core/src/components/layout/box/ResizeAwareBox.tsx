b0ab13524efde3fdaf0346a05bf61adde3fc98a0
Mattias Andersson <mattias800@gmail.com>
2020-06-05 10:03:33 +0200

    - Add UseElementDimensions hook. Can be used to get dimensions of an HTML element, and listen for changes to the dimensions. - Add ResizeAwareBox, which is a Box with added onResize callback prop. - Use ResizeAwareBox in Popover, to reposition the popover whenever it's content changes size.

---
+++ packages/core/src/components/layout/box/ResizeAwareBox.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { RefObject, useRef } from "react";
+import { Box, BoxProps } from "./Box";
+import {
+  ElementDimensions,
+  useElementDimensions
+} from "../../../hooks/UseElementDimensions";
+
+interface Props extends BoxProps {
+  onResize?: (dimensions: ElementDimensions) => void;
+}
+
+export const ResizeAwareBox: React.FC<Props> = ({
+  innerRef,
+  onResize,
+  ...boxProps
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const currentRef = (innerRef as RefObject<HTMLDivElement>) || ref;
+  useElementDimensions(currentRef, onResize);
+
+  return <Box {...boxProps} innerRef={currentRef} />;
+};
