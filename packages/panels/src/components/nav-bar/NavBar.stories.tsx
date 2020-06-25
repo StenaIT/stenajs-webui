5e46106a7f601a61768c273168410f7bfcccd299
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-06-25 17:19:43 +0200

    Center element in navbar (#132)

--- packages/panels/src/components/nav-bar/NavBar.stories.tsx
+++ packages/panels/src/components/nav-bar/NavBar.stories.tsx
@@ -1,11 +1,14 @@
 import { faAddressCard } from "@fortawesome/free-solid-svg-icons/faAddressCard";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons/faCoffee";
 import { faFire } from "@fortawesome/free-solid-svg-icons/faFire";
-import { Box, Row, StandardText } from "@stenajs-webui/core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons/faSearch";
+import { Box, Clickable, Row, StandardText } from "@stenajs-webui/core";
 import { Icon, WithBadge } from "@stenajs-webui/elements";
 import { NavBar, NavBarButton, NavBarMenuButton } from "@stenajs-webui/panels";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import { TextInput } from "@stenajs-webui/forms";
+import { ClassNames } from "@emotion/core";
 
 storiesOf("panels/NavBar", module)
   .add("standard", () => (
@@ -20,6 +23,42 @@ storiesOf("panels/NavBar", module)
       <NavBarButton label={"Customers"} leftIcon={faFire} selected />
       <NavBarButton label={"Bookings"} leftIcon={faCoffee} />
       <NavBarButton label={"Events"} leftIcon={faAddressCard} />
+    </NavBar>
+  ))
+  .add("with buttons and centered content", () => (
+    <NavBar
+      center={
+        <ClassNames>
+          {({ css }) => (
+            <TextInput
+              wrapperClassName={css`
+                --swui-field-border-color: var(--lhds-color-blue-900);
+                --swui-field-bg-enabled: var(--lhds-color-blue-900);
+                --swui-field-focus-shadow: inset 0px 0px 3pt 0pt
+                  rgba(255, 255, 255, 0.3);
+                --swui-field-border-color-hover: var(--lhds-color-ui-300);
+                --swui-field-text-color: var(--lhds-color-ui-50);
+                --swui-textinput-placeholder-color: var(--lhds-color-ui-50);
+
+                &:focus-within {
+                  --swui-field-text-color: var(--swui-field-text-color);
+                  --swui-textinput-bg-color: var(--lhds-color-ui-50);
+                }
+              `}
+              placeholder={"Search..."}
+              contentRight={
+                <Clickable>
+                  <Icon icon={faSearch} />
+                </Clickable>
+              }
+            />
+          )}
+        </ClassNames>
+      }
+    >
+      <NavBarButton label={"Customers"} selected />
+      <NavBarButton label={"Bookings"} />
+      <NavBarButton label={"Events"} />
     </NavBar>
   ))
   .add("with right buttons", () => (
