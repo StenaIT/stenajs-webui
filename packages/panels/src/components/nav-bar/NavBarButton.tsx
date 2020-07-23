8257f61aaf4352b1435beddbceaf68f9119b3f3f
Niklas Tegnander <niklas.tegnander@stenaline.com>
2020-07-23 12:40:40 +0200

    Fix NavBarButton types

--- packages/panels/src/components/nav-bar/NavBarButton.tsx
+++ packages/panels/src/components/nav-bar/NavBarButton.tsx
@@ -1,11 +1,11 @@
-import { FlatButtonProps, FlatButton } from "@stenajs-webui/elements";
+import { FlatButton, FlatButtonProps } from "@stenajs-webui/elements";
 import * as React from "react";
 import styles from "./NavBarButton.module.css";
 import cx from "classnames";
 
-export interface NavBarButtonProps extends FlatButtonProps {
+export type NavBarButtonProps = FlatButtonProps & {
   selected?: boolean;
-}
+};
 
 export const NavBarButton: React.FC<NavBarButtonProps> = ({
   selected,
