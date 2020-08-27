7e554ab0cfaa13c04d779e4fac01907863e3bc5b
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-08-27 10:05:18 +0200

    Prettier

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,9 @@
-import { withContexts } from '@storybook/addon-contexts/react';
+import { withContexts } from "@storybook/addon-contexts/react";
 import { withInfo } from "@storybook/addon-info";
 import { withKnobs } from "@storybook/addon-knobs";
 import { addDecorator, addParameters, configure } from "@storybook/react";
 import { StenaTheme } from "./stena-theme";
-import { contexts } from './configs/contexts';
+import { contexts } from "./configs/contexts";
 
 import { withA11y } from "@storybook/addon-a11y"; // Must be imported last, otherwise we get weird error.
 
