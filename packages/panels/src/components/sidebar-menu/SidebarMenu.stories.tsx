47e8731192a0ea1905c53d7b595eebf4595cb4ee
Mattias Andersson <mattias800@gmail.com>
2020-11-19 09:17:23 +0100

    - Remove alerts in stories.

--- packages/panels/src/components/sidebar-menu/SidebarMenu.stories.tsx
+++ packages/panels/src/components/sidebar-menu/SidebarMenu.stories.tsx
@@ -20,41 +20,25 @@ export default {
 };
 
 export const Overview = () => {
-  const onClick = () => alert("Click");
+  const noop = () => {};
 
   return (
     <Column width={"250px"}>
       <SidebarMenu onCloseClick={() => alert("Close menu")}>
         <SidebarMenuHeading label={"Product name"} />
         <SidebarMenuLink
-          onClick={() => alert("Clicked Customers")}
+          onClick={noop}
           leftIcon={faUserFriends}
           label={"Level 1.1"}
         />
-        <SidebarMenuLink
-          onClick={() => alert("Clicked Customers")}
-          label={"No icon"}
-        />
-        <SidebarMenuLink
-          onClick={() => alert("Clicked Customers")}
-          loading
-          label={"Spinner"}
-        />
-        <SidebarMenuLink
-          onClick={() => alert("Clicked Customers")}
-          selected
-          label={"Selected"}
-        />
+        <SidebarMenuLink onClick={noop} label={"No icon"} />
+        <SidebarMenuLink onClick={noop} loading label={"Spinner"} />
+        <SidebarMenuLink onClick={noop} selected label={"Selected"} />
         <SidebarMenuCollapsible label={"Level 1.2"} leftIcon={faChartBar}>
-          <SidebarMenuLink indent label={"Level 2.1"} onClick={onClick} />
-          <SidebarMenuLink indent label={"Level 2.2"} onClick={onClick} />
-          <SidebarMenuLink
-            indent
-            label={"Level 2.3"}
-            onClick={onClick}
-            selected
-          />
-          <SidebarMenuLink indent label={"Level 2.4"} onClick={onClick} />
+          <SidebarMenuLink indent label={"Level 2.1"} onClick={noop} />
+          <SidebarMenuLink indent label={"Level 2.2"} onClick={noop} />
+          <SidebarMenuLink indent label={"Level 2.3"} onClick={noop} selected />
+          <SidebarMenuLink indent label={"Level 2.4"} onClick={noop} />
         </SidebarMenuCollapsible>
 
         <SidebarMenuSeparator />
@@ -63,12 +47,12 @@ export const Overview = () => {
         <SidebarMenuLink
           leftIcon={faBook}
           label={"User manual"}
-          onClick={() => alert("Click on quick guide")}
+          onClick={noop}
         />
         <SidebarMenuLink
           leftIcon={faPaperPlane}
           label={"Contact"}
-          onClick={() => alert("Click on contact")}
+          onClick={noop}
         />
 
         <Box>
@@ -87,34 +71,24 @@ export const Overview = () => {
 };
 
 export const WithoutIcons = () => {
-  const onClick = () => alert("Click");
+  const noop = () => {};
   return (
     <Column width={300} height={800} justifyContent={"space-between"}>
-      <SidebarMenu onCloseClick={() => alert("Close menu")}>
+      <SidebarMenu onCloseClick={noop}>
         <SidebarMenuHeading label={"Freight Pricing Portal"} />
-        <SidebarMenuLink
-          selected
-          onClick={() => alert("Clicked Customers")}
-          label={"Customers"}
-        />
+        <SidebarMenuLink selected onClick={noop} label={"Customers"} />
         <SidebarMenuCollapsible label={"Statistics"}>
-          <SidebarMenuLink indent label={"Total"} onClick={onClick} />
-          <SidebarMenuLink indent label={"Sales region"} onClick={onClick} />
-          <SidebarMenuLink indent label={"Routes"} onClick={onClick} selected />
-          <SidebarMenuLink indent label={"Sellers"} onClick={onClick} />
+          <SidebarMenuLink indent label={"Total"} onClick={noop} />
+          <SidebarMenuLink indent label={"Sales region"} onClick={noop} />
+          <SidebarMenuLink indent label={"Routes"} onClick={noop} selected />
+          <SidebarMenuLink indent label={"Sellers"} onClick={noop} />
         </SidebarMenuCollapsible>
 
         <SidebarMenuSeparator />
 
         <SidebarMenuHeading label={"Support"} />
-        <SidebarMenuLink
-          label={"Quick guide"}
-          onClick={() => alert("Click on quick guide")}
-        />
-        <SidebarMenuLink
-          label={"Contact Team WebUI"}
-          onClick={() => alert("Click on contact")}
-        />
+        <SidebarMenuLink label={"Quick guide"} onClick={noop} />
+        <SidebarMenuLink label={"Contact Team WebUI"} onClick={noop} />
         <Box>
           <Box spacing={8}>
             <Icon color={"var(--swui-white)"} size={50} data-hover={true} />
