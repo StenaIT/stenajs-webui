c237506ee350d7404f4d7f05957966cbf27b6b72
Johan Lindskogen <johan.lindskogen@gmail.com>
2021-03-17 15:42:59 +0100

    Space is not using inline style anymore

--- packages/core/src/components/layout/space/Space.tsx
+++ packages/core/src/components/layout/space/Space.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import * as React from "react";
 
 export interface SpaceProps {
@@ -7,23 +8,28 @@ export interface SpaceProps {
   vertical?: boolean;
 }
 
+const InnerSpace = styled.div<{
+  num: number;
+  vertical: boolean;
+  horizontal: boolean;
+}>`
+  flex: none;
+  --current-size: ${({ num }) => num};
+  width: ${({ vertical }) =>
+    vertical ? "1px" : "calc(var(--current-size) * var(--swui-metrics-space))"};
+  height: ${({ horizontal }) =>
+    horizontal
+      ? "1px"
+      : "calc(var(--current-size) * var(--swui-metrics-space))"};
+`;
+
 export const Space: React.VFC<SpaceProps> = ({
   half = false,
   horizontal = false,
   num = 1,
   vertical = false,
 }) => {
-  const halfMod = half ? 0.5 : 1.0;
-  const size = num * halfMod;
-  return (
-    <div
-      style={{
-        width: vertical ? "1px" : `calc(${size} * var(--swui-metrics-space))`,
-        height: horizontal
-          ? "1px"
-          : `calc(${size} * var(--swui-metrics-space))`,
-        flex: "none",
-      }}
-    />
-  );
+  const size = num * (half ? 0.5 : 1);
+
+  return <InnerSpace num={size} horizontal={horizontal} vertical={vertical} />;
 };
