96e67ee79e37485111e7ceb25d5fb11786107d34
Niklas Tegnander <niklas.tegnander@stenaline.com>
2021-04-09 10:23:40 +0200

    Remove FC

--- packages/elements/src/components/ui/tabs/Tab.tsx
+++ packages/elements/src/components/ui/tabs/Tab.tsx
@@ -13,51 +13,49 @@ export interface TabProps extends ButtonElementProps, ButtonContentProps {
   selected?: boolean;
 }
 
-export const Tab: React.FC<TabProps> = forwardRef<HTMLButtonElement, TabProps>(
-  function Tab(
-    {
-      selected,
-      label,
-      loading = false,
-      left,
-      leftIcon,
-      right,
-      rightIcon,
-      success = false,
-      tabIndex = 0, // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Tab_Role
-      ...buttonProps
-    },
-    ref
-  ) {
-    const buttonLabel = getButtonLabel(
-      label,
-      success,
-      undefined,
-      loading,
-      undefined
-    );
+export const Tab = forwardRef<HTMLButtonElement, TabProps>(function Tab(
+  {
+    selected,
+    label,
+    loading = false,
+    left,
+    leftIcon,
+    right,
+    rightIcon,
+    success = false,
+    tabIndex = 0, // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Tab_Role
+    ...buttonProps
+  },
+  ref
+) {
+  const buttonLabel = getButtonLabel(
+    label,
+    success,
+    undefined,
+    loading,
+    undefined
+  );
 
-    return (
-      <button
-        className={cx(styles.tab, selected && styles.selected)}
-        role={"tab"}
-        aria-selected={selected}
-        tabIndex={tabIndex}
-        ref={ref}
-        {...buttonProps}
-      >
-        <div className={styles.inner}>
-          <ButtonContent
-            success={success}
-            loading={loading}
-            leftIcon={leftIcon}
-            left={left}
-            right={right}
-            rightIcon={rightIcon}
-            label={buttonLabel}
-          />
-        </div>
-      </button>
-    );
-  }
-);
+  return (
+    <button
+      className={cx(styles.tab, selected && styles.selected)}
+      role={"tab"}
+      aria-selected={selected}
+      tabIndex={tabIndex}
+      ref={ref}
+      {...buttonProps}
+    >
+      <div className={styles.inner}>
+        <ButtonContent
+          success={success}
+          loading={loading}
+          leftIcon={leftIcon}
+          left={left}
+          right={right}
+          rightIcon={rightIcon}
+          label={buttonLabel}
+        />
+      </div>
+    </button>
+  );
+});
