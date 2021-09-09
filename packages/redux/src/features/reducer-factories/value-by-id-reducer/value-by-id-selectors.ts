c3c69f356ab40278c9556972235aad8eaf63ed65
mattias800 <mattias800@gmail.com>
2021-09-09 14:29:13 +0200

    - Rename some selectors.

--- packages/redux/src/features/reducer-factories/value-by-id-reducer/value-by-id-selectors.ts
+++ packages/redux/src/features/reducer-factories/value-by-id-reducer/value-by-id-selectors.ts
@@ -1,15 +1,15 @@
 import { ValueByIdState } from "./value-by-id-reducer";
 
-export interface EntityByIdSelectors<TStoreState, TValue> {
-  getState: EntityByIdStateSelector<TStoreState, TValue>;
+export interface ValueByIdSelectors<TStoreState, TValue> {
+  getState: ValueByIdStateSelector<TStoreState, TValue>;
 }
 
-export type EntityByIdStateSelector<TStoreState, TValue> = (
+export type ValueByIdStateSelector<TStoreState, TValue> = (
   state: TStoreState
 ) => ValueByIdState<TValue>;
 
 export const createValueByIdSelectors = <TStoreState, TValue>(
-  stateSelector: EntityByIdStateSelector<TStoreState, TValue>
-): EntityByIdSelectors<TStoreState, TValue> => ({
+  stateSelector: ValueByIdStateSelector<TStoreState, TValue>
+): ValueByIdSelectors<TStoreState, TValue> => ({
   getState: stateSelector,
 });
