5ad35cbc37f2f653769587feeface5f245825cd9
Mattias Andersson <mattias800@gmail.com>
2021-11-25 12:53:51 +0100

    Chips are now more flexible. (#414)

---
+++ packages/filter/src/features/search-filter/features/chips/SearchFilterChip.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { useCallback } from "react";
+import { ChipSpacer } from "./ChipSpacer";
+import { Chip } from "@stenajs-webui/elements";
+import { useSearchFilterDispatch } from "../../context/SearchFilterDispatchContext";
+import { useSearchFilterActions } from "../../context/SearchFilterActionsContext";
+import { SearchFilterSectionOnClickRemoveOnChip } from "../../config/SearchFilterConfig";
+
+interface SearchFilterChipProps<TFormModel, TSectionKey> {
+  sectionId: TSectionKey;
+  label: string;
+  value: string;
+  onClickRemove?: SearchFilterSectionOnClickRemoveOnChip<TFormModel>;
+}
+
+export const SearchFilterChip = function SearchFilterChip<
+  TFormModel,
+  TSectionKey extends string
+>({
+  sectionId,
+  label,
+  value,
+  onClickRemove,
+}: SearchFilterChipProps<TFormModel, TSectionKey>) {
+  const dispatch = useSearchFilterDispatch();
+  const actions = useSearchFilterActions();
+
+  const setFormModelFields = useCallback(
+    (fields: Partial<TFormModel>) =>
+      dispatch(actions.setFormModelFields(fields)),
+    [dispatch, actions]
+  );
+
+  const onClickRemoveHandler = useCallback(() => {
+    onClickRemove?.({ value, setFormModelFields });
+  }, [onClickRemove, setFormModelFields, value]);
+
+  const onClickLabel = useCallback(() => {
+    dispatch(actions.clearExpandedSections());
+    dispatch(actions.expandSection(sectionId));
+    dispatch(actions.openFilters());
+  }, [actions, dispatch, sectionId]);
+
+  return (
+    <ChipSpacer>
+      <Chip
+        label={label ?? sectionId}
+        onClick={onClickLabel}
+        onClickRemove={onClickRemove ? onClickRemoveHandler : undefined}
+      />
+    </ChipSpacer>
+  );
+};
