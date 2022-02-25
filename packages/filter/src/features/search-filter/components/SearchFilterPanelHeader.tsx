e4b008a64a197914a208012171a1a1aad2e630ab
mattias800 <mattias800@gmail.com>
2022-02-25 08:58:36 +0100

    - Some design updates on search filter drawer and panel.

--- packages/filter/src/features/search-filter/components/SearchFilterPanelHeader.tsx
+++ packages/filter/src/features/search-filter/components/SearchFilterPanelHeader.tsx
@@ -1,20 +1,22 @@
 import * as React from "react";
 import { Heading, Indent, Row } from "@stenajs-webui/core";
-import { FlatButton, Icon } from "@stenajs-webui/elements";
+import { Icon, SecondaryButton } from "@stenajs-webui/elements";
 import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes";
 import { faSlidersH } from "@fortawesome/free-solid-svg-icons/faSlidersH";
 import { cssColor } from "@stenajs-webui/theme";
 
 interface SearchFilterPanelHeaderProps {
   onRequestClose: () => void;
+  header?: string;
 }
 
 export const SearchFilterPanelHeader: React.FC<SearchFilterPanelHeaderProps> = ({
   onRequestClose,
+  header = "Filters",
 }) => {
   return (
     <Row
-      height={"48px"}
+      height={"56px"}
       justifyContent={"space-between"}
       alignItems={"center"}
       indent={2}
@@ -22,13 +24,13 @@ export const SearchFilterPanelHeader: React.FC<SearchFilterPanelHeaderProps> = (
       <Row alignItems={"center"}>
         <Icon
           icon={faSlidersH}
-          color={cssColor("--lhds-color-ui-500")}
-          size={12}
+          color={cssColor("--lhds-color-ui-800")}
+          size={24}
         />
         <Indent />
-        <Heading variant={"h4"}>Filters</Heading>
+        <Heading variant={"h4"}>{header}</Heading>
       </Row>
-      <FlatButton leftIcon={faTimes} onClick={onRequestClose} />
+      <SecondaryButton leftIcon={faTimes} onClick={onRequestClose} />
     </Row>
   );
 };
