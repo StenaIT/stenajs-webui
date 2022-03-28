6e2410bcc9eedee3c8204849048e71ba59ea3749
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-03-28 12:09:43 +0200

    Update Card header (fixes AB#88134) (#443)

--- packages/elements/src/components/ui/cards/CardHeader.tsx
+++ packages/elements/src/components/ui/cards/CardHeader.tsx
@@ -1,14 +1,17 @@
-import { Heading, Indent, Row } from "@stenajs-webui/core";
+import { Heading, Row } from "@stenajs-webui/core";
 import cx from "classnames";
 import * as React from "react";
 import { ReactNode } from "react";
 import styles from "./CardHeader.module.css";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { Icon } from "../icon/Icon";
 
 export type CardHeaderVariant = "standard" | "compact";
 
 export interface CardHeaderProps {
   text?: string;
   variant?: CardHeaderVariant;
+  leftIcon?: IconDefinition;
   contentRight?: ReactNode;
   contentLeft?: ReactNode;
   contentCenter?: ReactNode;
@@ -18,6 +21,7 @@ export interface CardHeaderProps {
 export const CardHeader: React.VFC<CardHeaderProps> = ({
   text,
   variant = "standard",
+  leftIcon,
   contentAfterHeading,
   contentRight,
   contentLeft,
@@ -25,20 +29,15 @@ export const CardHeader: React.VFC<CardHeaderProps> = ({
 }) => {
   return (
     <div className={cx(styles.cardHeader, styles[variant])}>
-      <Row alignItems={"center"}>
-        {contentLeft && (
-          <>
-            {contentLeft}
-            <Indent />
-          </>
+      <Row alignItems={"center"} gap={variant === "compact" ? 1 : 2}>
+        {contentLeft}
+        {leftIcon && (
+          <Icon icon={leftIcon} size={variant === "compact" ? 16 : 24} />
         )}
         {text && (
-          <>
-            <Heading variant={variant === "compact" ? "h4" : "h3"}>
-              {text}
-            </Heading>
-            <Indent />
-          </>
+          <Heading variant={variant === "compact" ? "h5" : "h4"}>
+            {text}
+          </Heading>
         )}
         {contentAfterHeading}
       </Row>
