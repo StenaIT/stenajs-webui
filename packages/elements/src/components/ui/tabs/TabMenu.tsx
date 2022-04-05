9a43f87a8bf58024a21cba6ed90ccaa6e2ad2190
Niklas Tegnander <niklas.tegnander@stenaline.com>
2022-04-05 17:41:37 +0200

    Disable border by default

--- packages/elements/src/components/ui/tabs/TabMenu.tsx
+++ packages/elements/src/components/ui/tabs/TabMenu.tsx
@@ -4,16 +4,13 @@ import { Row } from "@stenajs-webui/core";
 import cx from "classnames";
 
 export interface TabMenuProps {
-  disableBorder?: boolean;
+  enableBorder?: boolean;
 }
 
-export const TabMenu: React.FC<TabMenuProps> = ({
-  children,
-  disableBorder,
-}) => {
+export const TabMenu: React.FC<TabMenuProps> = ({ children, enableBorder }) => {
   return (
     <Row
-      className={cx(styles.tabMenu, { [styles.withBorder]: !disableBorder })}
+      className={cx(styles.tabMenu, { [styles.withBorder]: enableBorder })}
       gap={2}
       role={"tablist"}
     >
