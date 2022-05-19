c2f2ab5d99aec8e7cb5523fffedabb4956e8bd6d
Mattias Andersson <mattias800@gmail.com>
2022-05-19 12:00:59 +0200

    - getDataProps is now an arrow function.

--- packages/core/src/utils/PropsForwarder.ts
+++ packages/core/src/utils/PropsForwarder.ts
@@ -1,14 +1,14 @@
 import { keys } from "lodash";
 
-export function getDataProps<T extends Record<string, unknown>>(
+export const getDataProps = <T extends Record<string, unknown>>(
   props: Record<string, unknown> | {}
-): T {
-  let propList = keys(props);
-  return propList.filter(isDataProp).reduce<T>((sum, key) => {
-    (sum as any)[key] = props[key];
-    return sum;
-  }, {} as T);
-}
+): T =>
+  keys(props)
+    .filter(isDataProp)
+    .reduce<T>((sum, key) => {
+      (sum as any)[key] = props[key];
+      return sum;
+    }, {} as T);
 
 const isDataProp = <TProp extends string>(propName: TProp): boolean =>
   propName.indexOf("data-") === 0 || propName.indexOf("aria-") === 0;
