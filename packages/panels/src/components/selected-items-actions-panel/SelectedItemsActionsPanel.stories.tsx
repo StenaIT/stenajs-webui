08d1594bb21e44767c7b4fa20dd0a253e2849bad
Mattias Andersson <mattias800@gmail.com>
2022-05-19 11:15:13 +0200

    - Add SelectedItemsActionsPanel component.

---
+++ packages/panels/src/components/selected-items-actions-panel/SelectedItemsActionsPanel.stories.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { SelectedItemsActionsPanel } from "./SelectedItemsActionsPanel";
+import { Indent, Row, Space, Text } from "@stenajs-webui/core";
+import { FlatButton, SecondaryButton, Tag } from "@stenajs-webui/elements";
+
+export default {
+  title: "panels/SelectedItemsActionsPanel",
+  component: SelectedItemsActionsPanel,
+};
+
+export const Demo = () => {
+  return <SelectedItemsActionsPanel numItemsSelected={3} />;
+};
+
+export const WithButtons = () => {
+  return (
+    <SelectedItemsActionsPanel
+      numItemsSelected={3}
+      afterLabelContent={<FlatButton label={"Clear all"} />}
+      rightContent={
+        <Row>
+          <SecondaryButton label={"Download"} />
+          <Space />
+          <SecondaryButton label={"Export"} />
+        </Row>
+      }
+    />
+  );
+};
+
+export const CustomLabel = () => {
+  return (
+    <SelectedItemsActionsPanel
+      label={"Great selection, man!"}
+      afterLabelContent={<FlatButton label={"Clear all"} />}
+      rightContent={
+        <Row>
+          <SecondaryButton label={"Download"} />
+          <Space />
+          <SecondaryButton label={"Export"} />
+        </Row>
+      }
+    />
+  );
+};
+
+export const CustomLabelContent = () => {
+  return (
+    <SelectedItemsActionsPanel
+      label={
+        <Row alignItems={"center"}>
+          <Text>Status</Text>
+          <Indent />
+          <Tag label={"Active"} />
+        </Row>
+      }
+      afterLabelContent={<FlatButton label={"Clear all"} />}
+      rightContent={
+        <Row>
+          <SecondaryButton label={"Download"} />
+          <Space />
+          <SecondaryButton label={"Export"} />
+        </Row>
+      }
+    />
+  );
+};
