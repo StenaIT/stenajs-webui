a80fd645ed2e0f4022b5c8b0d1a7469612d2bee5
Niklas Tegnander <niklas.tegnander@stenaline.com>
2022-05-31 16:34:21 +0200

    Allow custom icon

--- packages/panels/src/components/nav-bar/NavBarNotificationButton.tsx
+++ packages/panels/src/components/nav-bar/NavBarNotificationButton.tsx
@@ -3,11 +3,13 @@ import * as React from "react";
 import cx from "classnames";
 import styles from "./NavBarNotificationButton.module.css";
 import { faBell } from "@fortawesome/free-solid-svg-icons/faBell";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 
 export interface NavBarNotificationButtonProps
   extends Omit<FlatButtonProps, "leftIcon" | "rightIcon" | "label"> {
   count: number;
   unread: boolean;
+  icon?: IconDefinition;
 }
 
 export const NavBarNotificationButton: React.FC<NavBarNotificationButtonProps> = ({
@@ -16,6 +18,7 @@ export const NavBarNotificationButton: React.FC<NavBarNotificationButtonProps> =
   className,
   labelClassName,
   iconClassName,
+  icon = faBell,
   ...buttonProps
 }) => {
   const hasCount = count > 0;
@@ -23,7 +26,7 @@ export const NavBarNotificationButton: React.FC<NavBarNotificationButtonProps> =
   return (
     <FlatButton
       {...buttonProps}
-      leftIcon={faBell}
+      leftIcon={icon}
       className={cx(
         { [styles.unread]: unread },
         { [styles.hasCount]: hasCount },
