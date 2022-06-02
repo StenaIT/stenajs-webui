326c0856c0f75971d82a0423e01b536207e48d2c
Niklas <2521318+nikteg@users.noreply.github.com>
2022-06-02 13:01:09 +0200

    Nav: Notification button + animation (fixes AB#92377) (#459)

---
+++ packages/panels/src/components/nav-bar/NavBarNotificationButton.tsx
@@ -0,0 +1,41 @@
+import { FlatButton, FlatButtonProps } from "@stenajs-webui/elements";
+import * as React from "react";
+import cx from "classnames";
+import styles from "./NavBarNotificationButton.module.css";
+import { faBell } from "@fortawesome/free-solid-svg-icons/faBell";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+export interface NavBarNotificationButtonProps
+  extends Omit<FlatButtonProps, "leftIcon" | "rightIcon" | "label"> {
+  count: number;
+  unread: boolean;
+  icon?: IconDefinition;
+}
+
+export const NavBarNotificationButton: React.FC<NavBarNotificationButtonProps> = ({
+  count,
+  unread = false,
+  className,
+  labelClassName,
+  iconClassName,
+  icon = faBell,
+  ...buttonProps
+}) => {
+  const hasCount = count > 0;
+
+  return (
+    <FlatButton
+      {...buttonProps}
+      leftIcon={icon}
+      className={cx(
+        { [styles.unread]: unread },
+        { [styles.hasCount]: hasCount },
+        styles.navBarNotificationButton,
+        className
+      )}
+      label={hasCount ? String(count) : undefined}
+      labelClassName={cx(labelClassName, styles.label)}
+      iconClassName={cx(iconClassName, styles.icon)}
+    />
+  );
+};
