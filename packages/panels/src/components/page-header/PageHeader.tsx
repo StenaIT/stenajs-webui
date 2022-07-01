846e1604d6f019f46706b2a1d3063fedc91ca6bf
Josefina Andreasson <josefina.andreasson@stenaline.com>
2022-07-01 09:50:58 +0200

    Give PageHeader a white background (#473)

--- packages/panels/src/components/page-header/PageHeader.tsx
+++ packages/panels/src/components/page-header/PageHeader.tsx
@@ -1,6 +1,7 @@
 import { Box, Row, SeparatorLine, Space } from "@stenajs-webui/core";
 import * as React from "react";
 import { ReactNode } from "react";
+import { cssColor } from "@stenajs-webui/theme";
 
 export interface PageHeaderProps {
   renderBreadCrumbs?: () => ReactNode;
@@ -16,7 +17,7 @@ export const PageHeader: React.FC<PageHeaderProps> = ({
   children,
 }) => {
   return (
-    <Box shadow={"box"}>
+    <Box shadow={"box"} background={cssColor("--lhds-color-ui-50")}>
       <Box indent={3}>
         {renderBreadCrumbs && <Row spacing={1.25}>{renderBreadCrumbs()}</Row>}
         {renderPageHeading?.()}
