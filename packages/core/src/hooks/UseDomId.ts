3a9f30e237284fef5bc4f35431d7b09addc7bb1e
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-07-01 10:29:38 +0200

    Replace useDomId with useId

--- packages/core/src/hooks/UseDomId.ts
+++ packages/core/src/hooks/UseDomId.ts
@@ -4,6 +4,7 @@ let id = 0;
 const genId = (componentName?: string) =>
   `webui-${componentName ? componentName + "-" : ""}${++id}`;
 
+/** @deprecated use useId-hook from React 18 */
 export const useDomId = (componentName?: string): string => {
   const [id, setId] = useState<string | null>(() => genId(componentName));
   useEffect(() => setId(genId(componentName)), [componentName]);
