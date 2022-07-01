5d91774c316fcec09f08998074f57aa5bb778e95
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-07-01 09:00:33 +0200

    Make sure subpaths are treated as externals

--- vite.config.provider.ts
+++ vite.config.provider.ts
@@ -4,6 +4,17 @@ import react from "@vitejs/plugin-react";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export const createViteConfig = (pkg) => {
+  const externalPackages = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ];
+
+  // Creating regexes of the packages to make sure subpaths of the
+  // packages are also treated as external
+  const regexesOfPackages = externalPackages.map(
+    (packageName) => new RegExp(`^${packageName}(/.*)?`)
+  );
+
   return defineConfig({
     plugins: [svgr(), react(), cssInjectedByJsPlugin()],
     build: {
@@ -16,10 +27,7 @@ export const createViteConfig = (pkg) => {
           (format === "cjs" ? pkg.main : pkg.module).replace("dist/", ""),
       },
       rollupOptions: {
-        external: [
-          ...Object.keys(pkg.dependencies || {}),
-          ...Object.keys(pkg.peerDependencies || {}),
-        ],
+        external: regexesOfPackages,
       },
     },
   });
