fd04998cebca6aee39819a8a11ab5cb89cb2a4e3
Johan Lindskogen <johan.lindskogen@gmail.com>
2022-09-15 08:56:53 +0200

    Prettier

--- packages/elements/src/icons/ui/IconsUi.stories.tsx
+++ packages/elements/src/icons/ui/IconsUi.stories.tsx
@@ -12,43 +12,45 @@ export default {
 
 export const AllIconsDemo = () => {
   const [selectedIcon, setSelectedIcon] = useState(Object.values(allIcons)[0]);
-  return (<>
-    <Box
-    indent
-    spacing
-    style={{
-      display: "grid",
-      gap: "8px",
-      gridTemplateColumns: "repeat(auto-fill, minmax(80px, 1fr))",
-      background: cssColor("--lhds-color-ui-100"),
-    }}
-  >
-    {Object.keys(allIcons).map((iconName) => (
+  return (
+    <>
       <Box
-        gap
         indent
-        borderRadius={8}
-        style={{ aspectRatio: "1", cursor: "pointer" }}
-        background={"rgb(255, 255, 255)"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        hoverBackground={"rgb(255, 212, 59)"}
-        onClick={() => {
-          navigator.clipboard.writeText(iconName);
-          setSelectedIcon(allIcons[iconName]);
+        spacing
+        style={{
+          display: "grid",
+          gap: "8px",
+          gridTemplateColumns: "repeat(auto-fill, minmax(80px, 1fr))",
+          background: cssColor("--lhds-color-ui-100"),
         }}
       >
-        <Icon key={iconName} icon={allIcons[iconName]} size={24} />
-        <Txt
-          size={"small"}
-          wordBreak={"break-word"}
-          style={{ textAlign: "center" }}
-        >
-          {iconName}
-        </Txt>
+        {Object.keys(allIcons).map((iconName) => (
+          <Box
+            gap
+            indent
+            borderRadius={8}
+            style={{ aspectRatio: "1", cursor: "pointer" }}
+            background={"rgb(255, 255, 255)"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            hoverBackground={"rgb(255, 212, 59)"}
+            onClick={() => {
+              navigator.clipboard.writeText(iconName);
+              setSelectedIcon(allIcons[iconName]);
+            }}
+          >
+            <Icon key={iconName} icon={allIcons[iconName]} size={24} />
+            <Txt
+              size={"small"}
+              wordBreak={"break-word"}
+              style={{ textAlign: "center" }}
+            >
+              {iconName}
+            </Txt>
+          </Box>
+        ))}
       </Box>
-    ))}
-  </Box>
-  <IconDemo icon={selectedIcon} />
-  </>);
+      <IconDemo icon={selectedIcon} />
+    </>
+  );
 };
