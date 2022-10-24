cb22e72d25cfe866650b8dd74b1f826978cd635a
Mattias Andersson <mattias800@gmail.com>
2022-10-24 14:14:57 +0200

    Add `StickySearchButton` which can be used in filter under sections, to add a standardised search button.

--- packages/filter/src/features/search-filter/components/ClearFiltersButton.tsx
+++ packages/filter/src/features/search-filter/components/ClearFiltersButton.tsx
@@ -1,15 +1,15 @@
 import { FlatButton } from "@stenajs-webui/elements";
 import * as React from "react";
 import { useCallback } from "react";
-import { useSearchFilterDispatch } from "../context/SearchFilterDispatchContext";
 import { useSearchFilterActions } from "../context/SearchFilterActionsContext";
+import { useSearchFilterDispatch } from "../context/SearchFilterDispatchContext";
 
 interface ClearFiltersButtonProps {
   label?: string;
   onClick?: () => void;
 }
 
-export const ClearFiltersButton: React.VFC<ClearFiltersButtonProps> = ({
+export const ClearFiltersButton: React.FC<ClearFiltersButtonProps> = ({
   label = "Clear filters",
   onClick,
 }) => {
