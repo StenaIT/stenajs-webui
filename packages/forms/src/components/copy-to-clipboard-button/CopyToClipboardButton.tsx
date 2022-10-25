3a2f0e6c79ab05e5ab3fc98a890fda19065cce1b
Mattias Andersson <mattias800@gmail.com>
2022-10-25 08:40:34 +0200

    CopyToClipboardButton now requires string value, no implicit toString.

--- packages/forms/src/components/copy-to-clipboard-button/CopyToClipboardButton.tsx
+++ packages/forms/src/components/copy-to-clipboard-button/CopyToClipboardButton.tsx
@@ -5,27 +5,34 @@ import { Tooltip } from "@stenajs-webui/tooltip";
 import * as React from "react";
 import { useCallback } from "react";
 
-export interface CopyToClipboardButtonProps<T> {
-  value: T;
+export interface CopyToClipboardButtonProps {
+  value?: string;
   tooltipLabel?: string;
   size?: FlatButtonProps["size"];
 }
 
-export function CopyToClipboardButton<T>({
+export function CopyToClipboardButton({
   value,
   tooltipLabel = "Copied to clipboard!",
   size = "small",
-}: CopyToClipboardButtonProps<T>) {
+}: CopyToClipboardButtonProps) {
   const [visible, setVisible] = useTimeoutState(false, 2000);
 
   const onClick = useCallback(async () => {
-    await navigator.clipboard.writeText(String(value));
-    setVisible(true);
+    if (value != null) {
+      await navigator.clipboard.writeText(value);
+      setVisible(true);
+    }
   }, [setVisible, value]);
 
   return (
     <Tooltip visible={visible} label={tooltipLabel}>
-      <FlatButton size={size} onClick={onClick} leftIcon={faCopy} />
+      <FlatButton
+        size={size}
+        onClick={onClick}
+        leftIcon={faCopy}
+        disabled={value == null}
+      />
     </Tooltip>
   );
 }
