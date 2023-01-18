109b439acec903790f86b10b9165f2a0dd0a710a
David Zetterdahl <david.zetterdahl@stenaline.com>
2023-01-18 16:31:40 +0100

    Create InputFieldButton component

---
+++ packages/elements/src/components/ui/buttons/input-field/InputFieldButton.stories.tsx
@@ -0,0 +1,32 @@
+import { faJediOrder } from "@fortawesome/free-brands-svg-icons/faJediOrder";
+import { Column, Text, Space } from "@stenajs-webui/core";
+import * as React from "react";
+import { ButtonSize } from "../common/ButtonCommon";
+import { InputFieldButton } from "./InputFieldButton";
+
+const buttonSizes: Array<ButtonSize> = ["small", "medium", "large"];
+
+export default {
+  title: "elements/Buttons/InputFieldButton",
+};
+
+export const Standard = () => (
+  <Column spacing indent>
+    <Text size={"large"}>InputFieldButton icons</Text>
+    <Space num={4} />
+    <div style={{ display: "inline-block" }}>
+      {buttonSizes.map((size) => (
+        <>
+          <InputFieldButton
+            key={size}
+            icon={faJediOrder}
+            onClick={() => {
+              alert("woop woop");
+            }}
+          />
+          <Space />
+        </>
+      ))}
+    </div>
+  </Column>
+);
---
+++ packages/elements/src/components/ui/buttons/input-field/InputFieldButton.tsx
@@ -0,0 +1,27 @@
+import cx from "classnames";
+import * as React from "react";
+import { forwardRef } from "react";
+import { Icon } from "../../icon/Icon";
+import styles from "./InputFieldButton.module.css";
+
+export interface InputFieldProps {
+  className?: string;
+  icon?: any;
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const InputFieldButton = forwardRef<HTMLButtonElement, InputFieldProps>(
+  function InputFieldButton({ className, icon, disabled, onClick }, ref) {
+    return (
+      <button
+        ref={ref}
+        className={cx(styles.inputFieldButton, className)}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {icon && <Icon icon={icon} size={24} />}
+      </button>
+    );
+  }
+);
