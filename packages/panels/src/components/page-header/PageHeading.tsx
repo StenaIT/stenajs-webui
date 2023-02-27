1fd134676e3d78aadb1eac3138c16e5ce488f323
Josefina Andreasson <josefina.andreasson@stenaline.com>
2023-02-27 12:55:03 +0100

    Add headingLevel to PageHeading, default being h1 (#561)

--- packages/panels/src/components/page-header/PageHeading.tsx
+++ packages/panels/src/components/page-header/PageHeading.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { ReactNode } from "react";
-import { Heading, Row } from "@stenajs-webui/core";
+import { Heading, HeadingVariant, Row } from "@stenajs-webui/core";
 
 export type PageHeadingVariant = "compact" | "standard" | "relaxed";
 
@@ -8,6 +8,7 @@ interface PageHeadingProps {
   contentLeft?: ReactNode;
   contentRight?: ReactNode;
   heading: string;
+  headingLevel?: HeadingVariant;
   variant?: PageHeadingVariant;
 }
 
@@ -17,14 +18,17 @@ const variantToSpacing: Record<PageHeadingVariant, number> = {
   relaxed: 2,
 };
 
-export const PageHeading: React.VFC<PageHeadingProps> = ({
+export const PageHeading: React.FC<PageHeadingProps> = ({
   heading,
+  headingLevel = "h1",
   variant = "standard",
   contentLeft,
   contentRight,
 }) => (
   <Row spacing={variantToSpacing[variant]} alignItems={"center"} gap={2}>
-    <Heading variant={"h3"}>{heading}</Heading>
+    <Heading variant={"h3"} as={headingLevel}>
+      {heading}
+    </Heading>
     <Row alignItems={"center"}>{contentLeft}</Row>
     <Row style={{ marginLeft: "auto" }} alignItems={"center"}>
       {contentRight}
