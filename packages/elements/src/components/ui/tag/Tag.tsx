bbf4174c346f865252a8c6c1b9ba0f0595521038
Mattias Andersson <mattias800@gmail.com>
2023-03-02 14:02:46 +0100

    - New design for `Tag`. - Add `icon` prop to `Tag`.

--- packages/elements/src/components/ui/tag/Tag.tsx
+++ packages/elements/src/components/ui/tag/Tag.tsx
@@ -1,38 +1,40 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import * as React from "react";
 import cx from "classnames";
 import styles from "./Tag.module.css";
-import { getDataProps } from "@stenajs-webui/core";
+import { DivProps, getDataProps } from "@stenajs-webui/core";
 
 export interface TagProps {
   variant?: TagVariant;
   label?: string;
   size?: TagSize;
   className?: string;
+  style?: DivProps["style"];
+  icon?: IconDefinition;
 }
 
-export type TagVariant =
-  | "info"
-  | "error"
-  | "warning"
-  | "success"
-  | "passive"
-  | "turquoise";
+export type TagVariant = "info" | "error" | "warning" | "success" | "passive";
 
 export type TagSize = "medium" | "small";
 
 export const Tag: React.FC<TagProps> = ({
   className,
+  style,
   variant = "info",
   size = "medium",
   label,
+  icon,
   ...rest
 }) => {
   return (
     <div
       className={cx(styles.tag, styles[variant], styles[size], className)}
+      style={style}
       {...getDataProps(rest)}
     >
-      {label}
+      {icon && <FontAwesomeIcon icon={icon} />}
+      <span className={styles.label}>{label}</span>
     </div>
   );
 };
