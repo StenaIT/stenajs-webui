a292d951d025df1c84702d835d5d79398eabf67c
Mattias Andersson <mattias800@gmail.com>
2023-09-05 14:06:59 +0200

    - Use new blue colors. (#584)

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,6 +1,7 @@
 import "./styles/base.css";
 import "./styles/default-colors.css";
 import "./styles/default-theme.css";
+import "./styles/brand-colors.css";
 
 export * from "./util/CssColor";
 export * from "./types/ThemeColor";
