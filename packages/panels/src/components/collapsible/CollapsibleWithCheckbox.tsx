0e6de3954db84faeff24ef2cb85c250a1ab86182
RobertStigsson <robert.stigsson@gmail.com>
2023-09-06 13:43:18 +0200

    Fixed issue with new disabled functionality in CollapsibleWithCheckbox (#586)
    Co-authored-by: Niklas <2521318+nikteg@users.noreply.github.com>

--- packages/panels/src/components/collapsible/CollapsibleWithCheckbox.tsx
+++ packages/panels/src/components/collapsible/CollapsibleWithCheckbox.tsx
@@ -8,7 +8,7 @@ export interface CollapsibleWithCheckboxProps
       CheckboxProps,
       "value" | "onValueChange" | "onChange" | "indeterminate"
     > {
-  checkboxDisabled: boolean;
+  checkboxDisabled: CheckboxProps["disabled"];
 }
 
 export const CollapsibleWithCheckbox: React.FC<
