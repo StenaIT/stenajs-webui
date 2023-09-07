eb13502f8d43684203deb761dfa8799fcf03587b
Mattias Andersson <mattias800@gmail.com>
2023-09-07 12:03:23 +0200

    - Breaking: SidebarMenuHeading is now stricter, and does not allow contentLeft, contentRight or custom Text props.

--- packages/panels/src/components/sidebar-menu/SidebarMenuHeading.tsx
+++ packages/panels/src/components/sidebar-menu/SidebarMenuHeading.tsx
@@ -1,24 +1,17 @@
-import { Box, Space, Text, TextProps } from "@stenajs-webui/core";
+import { Box, Space, Text } from "@stenajs-webui/core";
 import * as React from "react";
-import { CollapsibleSimpleContentProps } from "../collapsible/CollapsibleContent";
 
-export interface SidebarMenuHeadingProps
-  extends Pick<CollapsibleSimpleContentProps, "contentLeft" | "contentRight">,
-    TextProps {
-  label?: string;
+export interface SidebarMenuHeadingProps {
+  label: string;
 }
 
 export const SidebarMenuHeading: React.FC<SidebarMenuHeadingProps> = ({
-  className,
   label,
-  contentLeft,
-  contentRight,
-  ...textProps
 }) => {
   return (
     <Box height={"32px"} justifyContent={"center"} indent={2}>
       <Space />
-      <Text variant={"overline"} size={"smaller"} {...textProps}>
+      <Text variant={"overline"} size={"smaller"}>
         {label}
       </Text>
     </Box>
