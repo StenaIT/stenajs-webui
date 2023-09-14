9071c06998a2da41e650f9f616c8f1fe5dbb01d5
Mattias Andersson <mattias800@gmail.com>
2023-09-14 10:59:31 +0200

    - Add new CircledIcon component. - Fix color prop on Cardy.

--- packages/elements/src/components/ui/icon/CircledIcon.stories.tsx
+++ packages/elements/src/components/ui/icon/CircledIcon.stories.tsx
@@ -5,8 +5,8 @@ import {
   faShip,
   faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
-import { Column, Indent, Row, Space, Text, Txt } from "@stenajs-webui/core";
-import { Icon, IconProps } from "./Icon";
+import { Column, Row, Text, Txt } from "@stenajs-webui/core";
+import { CircledIcon, CircledIconProps } from "./CircledIcon";
 import { Story } from "@storybook/react";
 import {
   colorListControl,
@@ -14,33 +14,35 @@ import {
 } from "../../../storybook-helpers/storybook-controls";
 
 export default {
-  title: "elements/Icon",
-  component: Icon,
+  title: "elements/CircledIcon",
+  component: CircledIcon,
   argTypes: {
     color: colorListControl,
     icon: disabledControl,
   },
 };
 
-export const Demo: Story<IconProps> = (props) => (
-  <Icon {...props} icon={faCog} />
+export const Demo: Story<CircledIconProps> = (props) => (
+  <CircledIcon {...props} icon={faCog} />
 );
 
-export const WithColor = () => <Icon icon={faCog} color={"red"} />;
+export const WithColor = () => (
+  <CircledIcon icon={faCog} iconColor={"--modern-red"} />
+);
 
-export const WithSpin = () => <Icon icon={faSpinner} spin={true} />;
+export const WithSpin = () => <CircledIcon icon={faSpinner} spin={true} />;
 
-export const WithPulse = () => <Icon icon={faSpinner} pulse={true} />;
+export const WithPulse = () => <CircledIcon icon={faSpinner} pulse={true} />;
 
 export const HorizontalFlip = () => (
   <Column gap={4}>
     <Row gap={2} alignItems={"center"}>
       <Text>Normal</Text>
-      <Icon icon={faBicycle} />
+      <CircledIcon icon={faBicycle} />
     </Row>
     <Row gap={2} alignItems={"center"}>
       <Text>Flipped</Text>
-      <Icon icon={faBicycle} flip={"horizontal"} />
+      <CircledIcon icon={faBicycle} flip={"horizontal"} />
     </Row>
   </Column>
 );
@@ -49,11 +51,11 @@ export const VerticalFlip = () => (
   <Column gap={4}>
     <Row gap={2} alignItems={"center"}>
       <Text>Normal</Text>
-      <Icon icon={faBicycle} />
+      <CircledIcon icon={faBicycle} />
     </Row>
     <Row gap={2} alignItems={"center"}>
       <Text>Flipped</Text>
-      <Icon icon={faBicycle} flip={"vertical"} />
+      <CircledIcon icon={faBicycle} flip={"vertical"} />
     </Row>
   </Column>
 );
@@ -62,33 +64,33 @@ export const WithRotation = () => (
   <Row gap={2}>
     <Column alignItems={"center"} gap={2}>
       <Text>90</Text>
-      <Icon icon={faShip} rotation={90} />
+      <CircledIcon icon={faShip} rotation={90} />
     </Column>
     <Column alignItems={"center"} gap={2}>
       <Text>180</Text>
-      <Icon icon={faShip} rotation={180} />
+      <CircledIcon icon={faShip} rotation={180} />
     </Column>
     <Column alignItems={"center"} gap={2}>
       <Text>270</Text>
-      <Icon icon={faShip} rotation={270} />
+      <CircledIcon icon={faShip} rotation={270} />
     </Column>
   </Row>
 );
 
 export const WithTransform = () => (
-  <Icon icon={faBicycle} transform={{ flipX: true, rotate: 90 }} />
+  <CircledIcon icon={faBicycle} transform={{ flipX: true, rotate: 90 }} />
 );
 
 export const WithDisplay = () => (
   <>
     <p>
       <Txt>This </Txt>
-      <Icon icon={faBicycle} display={"inline"} />
+      <CircledIcon icon={faBicycle} display={"inline"} />
       <Txt> is inline</Txt>
     </p>
     <p style={{ display: "inline-block" }}>
       <Txt>This </Txt>
-      <Icon icon={faBicycle} />
+      <CircledIcon icon={faBicycle} />
       <Txt> is not inline</Txt>
     </p>
   </>
