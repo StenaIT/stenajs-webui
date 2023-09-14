5529c1e806abe22505ad6a5e8e9f0b9b563dc70a
Mattias Andersson <mattias800@gmail.com>
2023-09-14 08:50:40 +0200

    New component: Cardy (#597)
    Co-authored-by: Niklas <2521318+nikteg@users.noreply.github.com>

---
+++ packages/elements/src/components/ui/cardy/Cardy.tsx
@@ -0,0 +1,46 @@
+import { Box, BoxProps } from "@stenajs-webui/core";
+import * as React from "react";
+import { PropsWithChildren } from "react";
+import styles from "./Cardy.module.css";
+import { cssColor, CssPropColor } from "@stenajs-webui/theme";
+import { Spinner } from "../spinner/Spinner";
+import cx from "classnames";
+
+interface CardyProps extends Pick<BoxProps, "minWidth"> {
+  color?: CssPropColor;
+  loading?: boolean;
+  className?: string;
+}
+
+export const Cardy: React.FC<PropsWithChildren<CardyProps>> = ({
+  children,
+  color = cssColor("--lhds-color-ui-200"),
+  loading,
+  className,
+  ...restProps
+}) => {
+  return (
+    <Box
+      {...restProps}
+      background={color}
+      position={loading ? "relative" : undefined}
+      className={cx(styles.cardy, className)}
+    >
+      {children}
+      {loading && (
+        <Box
+          top={0}
+          bottom={0}
+          left={0}
+          right={0}
+          position={"absolute"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          background={"var(--swui-overlay-bg-color-light)"}
+        >
+          <Spinner />
+        </Box>
+      )}
+    </Box>
+  );
+};
