7fc2ef8d80d614848a2d5a40c885ada6c721aaee
Mattias Andersson <mattias800@gmail.com>
2023-09-14 08:37:45 +0200

    Use `PropsWithChildren`

--- packages/elements/src/components/ui/cardy/CardyBody.tsx
+++ packages/elements/src/components/ui/cardy/CardyBody.tsx
@@ -1,12 +1,12 @@
 import { Box, BoxProps } from "@stenajs-webui/core";
 import * as React from "react";
-import { ReactNode } from "react";
+import { PropsWithChildren } from "react";
 import styles from "./Cardy.module.css";
 
-export interface CardyBodyProps extends Pick<BoxProps, "gap"> {
-  children?: ReactNode;
-}
+export interface CardyBodyProps extends Pick<BoxProps, "gap"> {}
 
-export const CardyBody: React.FC<CardyBodyProps> = (boxProps) => {
+export const CardyBody: React.FC<PropsWithChildren<CardyBodyProps>> = (
+  boxProps
+) => {
   return <Box className={styles.cardyIndent} {...boxProps} />;
 };
