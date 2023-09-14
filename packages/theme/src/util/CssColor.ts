61df82b577a642d0d398c2cdc43d8f6e4325c4a1
Mattias Andersson <mattias800@gmail.com>
2023-09-14 09:01:07 +0200

    Update mussla color (#598)

--- packages/theme/src/util/CssColor.ts
+++ packages/theme/src/util/CssColor.ts
@@ -1,6 +1,7 @@
 import { LhdsColor } from "../types/LhdsColor";
 import { ThemeColor } from "../types/ThemeColor";
+import { BrandColor } from "../types/BrandColor";
 
-export type CssPropColor = LhdsColor | ThemeColor;
+export type CssPropColor = LhdsColor | ThemeColor | BrandColor;
 
 export const cssColor = (cssPropColor: CssPropColor) => `var(${cssPropColor})`;
