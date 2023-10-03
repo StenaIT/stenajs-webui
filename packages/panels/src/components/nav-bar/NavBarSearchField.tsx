6dc168d0eff3e0e9a392a221ee567c01d67a7a86
Mattias Andersson <mattias800@gmail.com>
2023-10-03 13:54:56 +0200

    - TextInput placeholder is now font-weight 500. - Placeholder disappears when input is focused. - Update style of NavBarSearchField as per design.

--- packages/panels/src/components/nav-bar/NavBarSearchField.tsx
+++ packages/panels/src/components/nav-bar/NavBarSearchField.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import { TextInput, TextInputProps } from "@stenajs-webui/forms";
-import { Clickable } from "@stenajs-webui/core";
-import { Icon, stenaSearch } from "@stenajs-webui/elements";
 import cx from "classnames";
 import styles from "./NavBarSearchField.module.css";
 
 interface NavBarSearchFieldProps extends TextInputProps {}
 
 export const NavBarSearchField: React.FC<NavBarSearchFieldProps> = ({
-  placeholder = "Search...",
+  placeholder = "Search",
   className,
   wrapperClassName,
   ...textInputProps
@@ -18,11 +16,6 @@ export const NavBarSearchField: React.FC<NavBarSearchFieldProps> = ({
       wrapperClassName={cx(styles.navBarSearchField, wrapperClassName)}
       className={cx(styles.navBarSearchFieldInput, className)}
       placeholder={placeholder}
-      contentRight={
-        <Clickable>
-          <Icon icon={stenaSearch} />
-        </Clickable>
-      }
       {...textInputProps}
     />
   );
