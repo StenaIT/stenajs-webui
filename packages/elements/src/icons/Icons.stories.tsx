a99057e0dd12da135b206d75826f6a477479f02d
Mattias Andersson <mattias800@gmail.com>
2023-10-20 12:50:19 +0200

    Add passenger type icons (#632)

--- packages/elements/src/icons/Icons.stories.tsx
+++ packages/elements/src/icons/Icons.stories.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { IconDemoList } from "./IconsDemo";
 import * as commonIcons from "./generated/CommonIcons";
 import * as vehicleTypeIcons from "./generated/VehicleTypeIcons";
+import * as passengerTypeIcons from "./generated/PassengerTypeIcons";
 
 export default {
   title: "elements/Icons",
@@ -13,4 +14,8 @@ export const CommonIcons = () => {
 
 export const VehicleTypeIcons = () => {
   return <IconDemoList icons={vehicleTypeIcons} />;
+};
+
+export const PassengerTypeIcons = () => {
+  return <IconDemoList icons={passengerTypeIcons} />;
 };
