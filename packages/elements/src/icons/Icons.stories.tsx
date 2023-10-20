66380dad7b21a9e048c9d5c692a9b97cd6354d43
Mattias Andersson <mattias800@gmail.com>
2023-10-20 08:46:39 +0200

    - New icon category for vehicle types. - Add vehicle type icons. - Add new common icons "compare" and "performance". - Update "sortUp" icon with fixed version.

--- packages/elements/src/icons/Icons.stories.tsx
+++ packages/elements/src/icons/Icons.stories.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { IconDemoList } from "./IconsDemo";
 import * as commonIcons from "./generated/CommonIcons";
+import * as vehicleTypeIcons from "./generated/VehicleTypeIcons";
 
 export default {
   title: "elements/Icons",
@@ -8,4 +9,8 @@ export default {
 
 export const CommonIcons = () => {
   return <IconDemoList icons={Object.values(commonIcons)} />;
+};
+
+export const VehicleTypeIcons = () => {
+  return <IconDemoList icons={Object.values(vehicleTypeIcons)} />;
 };
