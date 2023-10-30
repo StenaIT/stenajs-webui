2c0ccd41b753130910e49762fa3d5f776033ee23
Mattias Andersson <mattias800@gmail.com>
2023-10-30 16:43:10 +0100

    - Update story.

--- packages/forms/src/components/ui/labelled-text-input/LabelledTextInput.stories.tsx
+++ packages/forms/src/components/ui/labelled-text-input/LabelledTextInput.stories.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Story } from "@storybook/react";
-import { LabelledTextInput, LabelledTextInputProps } from "./LabelledTextInput";
+import { LabelledTextInput } from "./LabelledTextInput";
 import { Column, Heading } from "@stenajs-webui/core";
 
 export default {
@@ -8,7 +7,7 @@ export default {
   component: LabelledTextInput,
 };
 
-const Example = () => (
+export const Demo = () => (
   <Column gap={2}>
     <Heading>Medium</Heading>
     <LabelledTextInput
@@ -47,12 +46,43 @@ const Example = () => (
   </Column>
 );
 
-export const Desktop: Story<LabelledTextInputProps> = (props) => (
-  <Example {...props} />
-);
-
-export const Mobile: Story<LabelledTextInputProps> = (props) => (
-  <Example {...props} />
+export const Mobile = () => (
+  <Column gap={2}>
+    <Heading>Medium</Heading>
+    <LabelledTextInput
+      label={"Input field"}
+      placeholder={"Input field"}
+      size={"medium"}
+      id={"testmedium"}
+    />
+    <Heading>Large</Heading>
+    <LabelledTextInput
+      label={"Input field"}
+      placeholder={"Input field"}
+      size={"large"}
+      id={"testlarge"}
+    />
+    <Heading>Error</Heading>
+    <LabelledTextInput
+      label={"Input field"}
+      placeholder={"Input field"}
+      variant={"error"}
+      id={"testlarge"}
+    />
+    <Heading>Combined</Heading>
+    <Column>
+      <LabelledTextInput
+        id={"testto"}
+        label={"To"}
+        borderRadiusVariant={"onlyTop"}
+      />
+      <LabelledTextInput
+        id={"testfrom"}
+        label={"From"}
+        borderRadiusVariant={"onlyBottom"}
+      />
+    </Column>
+  </Column>
 );
 
 Mobile.parameters = {
