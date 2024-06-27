4fde03c6e043be945273fb1091bec295bdc7155a
Mattias Andersson <mattias800@gmail.com>
2024-06-27 14:53:29 +0200

    - Add story with all locales.

--- packages/calendar/src/components/input-types/travel-date-input/TravelDateInput.stories.tsx
+++ packages/calendar/src/components/input-types/travel-date-input/TravelDateInput.stories.tsx
@@ -2,6 +2,8 @@ import { Story } from "@storybook/react";
 import * as React from "react";
 import { useState } from "react";
 import { TravelDateInput, TravelDateInputValue } from "./TravelDateInput";
+import { Column, Row } from "@stenajs-webui/core";
+import { Banner } from "@stenajs-webui/elements";
 
 export default {
   title: "calendar/Input/TravelDateInput",
@@ -31,18 +33,48 @@ export const Standard = () => {
   );
 };
 
-export const UnitedKingdom = () => {
+export const Locales = () => {
+  const locales = [
+    "sv",
+    "da",
+    "en-GB",
+    "pl",
+    "nl",
+    "en-US",
+    "de-AT",
+    "de-DE",
+    "fr",
+    "de",
+    "es",
+    "nb",
+  ];
+
+  return (
+    <Column gap={4}>
+      {locales.map((locale) => (
+        <LocaleDemo localeCode={locale} />
+      ))}
+    </Column>
+  );
+};
+
+const LocaleDemo = ({ localeCode }: { localeCode: string }) => {
   const [value, setValue] = useState<TravelDateInputValue | undefined>(
     undefined
   );
 
   return (
-    <div style={{ display: "inline-block" }}>
-      <TravelDateInput
-        value={value}
-        onValueChange={setValue}
-        localeCode={"en-GB"}
-      />
-    </div>
+    <Column gap={2}>
+      <Row>
+        <Banner headerText={"Locale: " + localeCode} variant={"info"} />
+      </Row>
+      <Row>
+        <TravelDateInput
+          value={value}
+          onValueChange={setValue}
+          localeCode={localeCode}
+        />
+      </Row>
+    </Column>
   );
 };
