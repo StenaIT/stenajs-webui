491d1082b9e671eb4e139c2fae8c68ae09d1d61d
Mattias Andersson <mattias800@gmail.com>
2024-08-15 13:52:56 +0200

    Add size variant prop to RadioButtonBox (#764)
    - Add size variant prop to RadioButtonBox.
    - Add large size.

--- packages/forms/src/components/ui/radio/RadioButtonBox.stories.tsx
+++ packages/forms/src/components/ui/radio/RadioButtonBox.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { RadioButtonBox } from "./RadioButtonBox";
-import { Column, Text } from "@stenajs-webui/core";
+import { Column, Heading, Text } from "@stenajs-webui/core";
 import {
   stenaAnimals,
   stenaRunningPerson,
@@ -12,44 +12,58 @@ export default {
   component: RadioButtonBox,
 };
 
-export const Overview = () => (
-  <Column gap={2} width={"300px"}>
-    <RadioButtonBox
-      label={"On foot"}
-      name={"meansOfTravel"}
-      value={"onFoot"}
-      icon={stenaRunningPerson}
-    />
-    <RadioButtonBox
-      label={"With vehicle"}
-      name={"meansOfTravel"}
-      value={"withVehicle"}
-      icon={stenaVehicleCarStandard}
-    />
-    <RadioButtonBox
-      label={"With sharks"}
-      name={"meansOfTravel"}
-      value={"withSharks"}
-      variant={"danger"}
-      icon={stenaAnimals}
-    />
-    <RadioButtonBox
-      label={"With content right"}
-      name={"meansOfTravel"}
-      value={"withContentRight"}
-      contentRight={<Text>Hello world</Text>}
-    />
-    <RadioButtonBox
-      label={"With styling"}
-      name={"meansOfTravel"}
-      value={"withBg"}
-      style={{
-        ["--swui-radio-button-box-background" as string]:
-          "var(--lhds-color-blue-200)",
-        ["--swui-radio-button-box-border-color" as string]:
-          "var(--lhds-color-blue-300)",
-      }}
-      icon={stenaAnimals}
-    />
-  </Column>
-);
+export const Overview = () => {
+  const sizes = ["medium", "large"] as const;
+
+  return (
+    <Column gap={4}>
+      {sizes.map((size) => (
+        <Column gap={2} width={"300px"}>
+          <Heading>{size}</Heading>
+          <RadioButtonBox
+            size={size}
+            label={"On foot"}
+            name={"meansOfTravel"}
+            value={"onFoot"}
+            icon={stenaRunningPerson}
+          />
+          <RadioButtonBox
+            size={size}
+            label={"With vehicle"}
+            name={"meansOfTravel"}
+            value={"withVehicle"}
+            icon={stenaVehicleCarStandard}
+          />
+          <RadioButtonBox
+            size={size}
+            label={"With sharks"}
+            name={"meansOfTravel"}
+            value={"withSharks"}
+            variant={"danger"}
+            icon={stenaAnimals}
+          />
+          <RadioButtonBox
+            size={size}
+            label={"With content right"}
+            name={"meansOfTravel"}
+            value={"withContentRight"}
+            contentRight={<Text>Hello world</Text>}
+          />
+          <RadioButtonBox
+            size={size}
+            label={"With styling"}
+            name={"meansOfTravel"}
+            value={"withBg"}
+            style={{
+              ["--swui-radio-button-box-background" as string]:
+                "var(--lhds-color-blue-200)",
+              ["--swui-radio-button-box-border-color" as string]:
+                "var(--lhds-color-blue-300)",
+            }}
+            icon={stenaAnimals}
+          />
+        </Column>
+      ))}
+    </Column>
+  );
+};
