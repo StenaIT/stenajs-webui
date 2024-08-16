4ab8f2b5556b57cda6a2af670f355874bf153db5
Mattias Andersson <mattias800@gmail.com>
2024-08-16 10:58:05 +0200

    - Add large heading size variant to ModalHeading. - ModalHeader can specify what heading size to pass down to ModalHeading. - Same with heading level (what DOM element to use, h1, h2, etc).

--- packages/modal/src/building-blocks/ModalHeader.tsx
+++ packages/modal/src/building-blocks/ModalHeader.tsx
@@ -1,20 +1,28 @@
 import * as React from "react";
-import { Row } from "@stenajs-webui/core";
-import { ModalHeading } from "./ModalHeading";
+import { HeadingVariant, Row } from "@stenajs-webui/core";
+import { ModalHeading, ModalHeadingSizeVariant } from "./ModalHeading";
 import { CloseButton } from "@stenajs-webui/elements";
 
 export interface ModalHeaderProps {
   heading?: string;
+  headingSize?: ModalHeadingSizeVariant;
+  headingLevel?: HeadingVariant;
   onClickClose?: () => void;
 }
 
 export const ModalHeader: React.FC<ModalHeaderProps> = ({
   heading,
+  headingLevel,
+  headingSize,
   onClickClose,
 }) => {
   return (
     <Row justifyContent={"space-between"}>
-      {heading && <ModalHeading>{heading}</ModalHeading>}
+      {heading && (
+        <ModalHeading headingLevel={headingLevel} size={headingSize}>
+          {heading}
+        </ModalHeading>
+      )}
       <CloseButton onClick={onClickClose} />
     </Row>
   );
