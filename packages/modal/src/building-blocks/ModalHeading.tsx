2f612ce7a82ec3e07e10058f34a621cebea5b37a
Mattias Andersson <mattias800@gmail.com>
2024-08-16 11:05:42 +0200

    Add large heading size variant to ModalHeading (#767)
    - Add large heading size variant to ModalHeading.
    - ModalHeader can specify what heading size to pass down to ModalHeading.
    - Same with heading level (what DOM element to use, h1, h2, etc).

--- packages/modal/src/building-blocks/ModalHeading.tsx
+++ packages/modal/src/building-blocks/ModalHeading.tsx
@@ -1,13 +1,21 @@
 import * as React from "react";
-import { Heading } from "@stenajs-webui/core";
+import { Heading, HeadingVariant } from "@stenajs-webui/core";
+
+export type ModalHeadingSizeVariant = "medium" | "large";
 
 export interface ModalHeadingProps {
+  size?: ModalHeadingSizeVariant;
   children: string;
+  headingLevel?: HeadingVariant;
 }
 
-export const ModalHeading: React.FC<ModalHeadingProps> = ({ children }) => {
+export const ModalHeading: React.FC<ModalHeadingProps> = ({
+  headingLevel = "h2",
+  size = "medium",
+  children,
+}) => {
   return (
-    <Heading variant={"h3"} as={"h2"}>
+    <Heading variant={size === "medium" ? "h3" : "h1"} as={headingLevel}>
       {children}
     </Heading>
   );
