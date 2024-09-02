9f00e0a67ba550aa68a182a641d4959fd0980c34
Mattias Andersson <mattias800@gmail.com>
2024-09-02 15:28:39 +0200

    - Update date description.

--- packages/calendar/src/features/travel-calendar/util/DateDescriptionFormatter.ts
+++ packages/calendar/src/features/travel-calendar/util/DateDescriptionFormatter.ts
@@ -1,7 +1,17 @@
 import { format, Locale } from "date-fns";
 
-export const formatDateDescription = (date: Date, locale: Locale) =>
-  format(date, "eee MMM do", { locale });
+export const formatDateDescription = (date: Date, locale: Locale) => {
+  if (locale.code.startsWith("en")) {
+    return format(date, "eee MMM d", { locale });
+  } else {
+    return format(date, "eee d MMM", { locale });
+  }
+};
 
-export const formatDateDescriptionLong = (date: Date, locale: Locale) =>
-  format(date, "eeee MMMM do", { locale });
+export const formatDateDescriptionLong = (date: Date, locale: Locale) => {
+  if (locale.code.startsWith("en")) {
+    return format(date, "eeee MMMM d", { locale });
+  } else {
+    return format(date, "eeee d MMMM", { locale });
+  }
+};
